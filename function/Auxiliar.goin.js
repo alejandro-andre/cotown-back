6fa9bff86962986bcdf2022f98f0f73ddc3aa9dc
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-14 11:00:10 +0200

    Functions

---
+++ function/Auxiliar.goin.js
@@ -0,0 +1,2 @@
+var url = routingContext.request().getParam("url");
+routingContext.response().setStatusCode(200).putHeader("Location", url.toString()).end();
---
+++ function/Auxiliar.goout.js
@@ -0,0 +1,4 @@
+var url = routingContext.request().getParam("url");
+var token = routingContext.request().getParam("access_token");
+if (token != "") url += "?access_token=" + token;
+routingContext.response().setStatusCode(200).putHeader("Location", url.toString()).end();
