a43d050ea8e9930b9a5935f25c2b3ec035c9bf88
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-15 23:07:39 +0200

    Workflow changes

--- function/Billing.Payment_B1_process_payment.sql
+++ function/Billing.Payment_B1_process_payment.sql
@@ -1,9 +1,9 @@
--- Procesa cambios en los pagos
 DECLARE
 
   invoice_id INTEGER;
   customer_id INTEGER;
   status_record VARCHAR;
+  y VARCHAR;
 
 BEGIN
 
@@ -19,7 +19,9 @@ BEGIN
   END IF;
 
   -- Pago manual (sin auth code)
-  IF (NEW."Payment_auth" IS NULL) THEN 
+  IF (NEW."Payment_auth" IS NULL) THEN
+	  SELECT to_char(now(), 'YY') INTO y;
+    NEW."Payment_order" := CONCAT(y, LPAD(NEW.id::text, 5, '0'), '00000');
     NEW."Payment_auth" := 'MANUAL';
   END IF;
 
@@ -35,13 +37,13 @@ BEGIN
     -- Registra el pago
     INSERT INTO "Booking"."Booking_log" ("Booking_id", "Log") VALUES (NEW."Booking_id", 'Booking fee pagado');
 
-    -- Crea la factura
-    SELECT "Customer_id" INTO customer_id FROM "Booking"."Booking" WHERE id = NEW."Booking_id";
-    INSERT INTO "Billing"."Invoice" 
-    ("Bill_type", "Issued", "Issued_date", "Provider_id", "Customer_id", "Booking_id", "Details", "Payment_method_id", "Payment_id") 
-    VALUES 
-    ('factura', TRUE, CURRENT_DATE, 1, customer_id, NEW."Booking_id", 'Booking fee', NEW."Payment_method_id", NEW.id)
-    RETURNING id INTO invoice_id;
+    -- Crea la factura - Pasar al proceso de facturación
+    -- SELECT "Customer_id" INTO customer_id FROM "Booking"."Booking" WHERE id = NEW."Booking_id";
+    -- INSERT INTO "Billing"."Invoice" 
+    --   ("Bill_type", "Issued", "Issued_date", "Provider_id", "Customer_id", "Booking_id", "Details", "Payment_method_id", "Payment_id") 
+    --   VALUES ('factura', TRUE, CURRENT_DATE, 1, customer_id, NEW."Booking_id", 'Booking fee', NEW."Payment_method_id", NEW.id)
+    --   RETURNING id INTO invoice_id;
+    -- INSERT INTO "Billing"."Invoice_line" ("Invoice_id", "Product_id", "Amount", "Tax_id", "Details") VALUES (invoice_id, 1, NEW."Amount", 1, 'Booking fee');
 
     -- Comprobamos si el estado es 'solicitud'
     IF (status_record = 'solicitud') THEN
