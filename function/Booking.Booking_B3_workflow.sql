6510994e47b419eb6610cdee51b3d02a76565b37
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-16 20:15:27 +0200

    Workflow fixes and payment ok/ko pages

--- function/Booking.Booking_B3_workflow.sql
+++ function/Booking.Booking_B3_workflow.sql
@@ -7,29 +7,27 @@ BEGIN
 
   -- Actualiza al estado 'Pendiente de pago' cuando se asigna el recurso a una solicitud no pagada
   IF ((NEW."Status" = 'solicitud' OR NEW."Status" = 'alternativas') AND NEW."Resource_id" IS NOT NULL) THEN
-
-    -- Cambia el estado
     NEW."Status" :='pendientepago';
     NEW."Expiry_date" := (CURRENT_DATE + INTERVAL '2 days');
     change := 'Pendiente de pago';
-
   END IF;
   
   -- Actualiza al estado 'Confirmada' cuando se asigna el recurso a una solicitud pagada
   IF ((NEW."Status" = 'solicitudpagada' OR NEW."Status" = 'alternativaspagada') AND NEW."Resource_id" IS NOT NULL) THEN
-
-    -- Cambia el estado
     NEW."Status" := 'confirmada';
     NEW."Expiry_date" = NULL;
     change := 'Reserva confirmada';
-
-    -- Pago de garantía pendiente
-    INSERT INTO "Billing"."Payment"("Payment_method_id", "Booking_id", "Amount", "Issued_date", "Concept", "Payment_type" ) VALUES ('1', NEW.id, NEW."Deposit", CURRENT_DATE, 'Booking deposit', 'deposito');
-
   END IF;
 
-  -- Otros cambios de estado
+  -- Cambios de estado
   IF (NEW."Status" <> OLD."Status") THEN
+
+    -- Confirmada, Pago de garantía pendiente
+    IF (NEW."Status" = 'confirmada' ) THEN
+      INSERT INTO "Billing"."Payment"("Payment_method_id", "Booking_id", "Amount", "Issued_date", "Concept", "Payment_type" ) 
+        VALUES ('1', NEW.id, NEW."Deposit", CURRENT_DATE, 'Booking deposit', 'deposito');
+    END IF;
+
   END IF;
 
   -- Registra el cambio
