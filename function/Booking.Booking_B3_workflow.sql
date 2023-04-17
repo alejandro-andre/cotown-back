a737c1dfa34de0de5b6d61de8b1e2374370dc562
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-17 11:57:22 +0200

    Fix

--- function/Booking.Booking_B3_workflow.sql
+++ function/Booking.Booking_B3_workflow.sql
@@ -10,29 +10,62 @@ BEGIN
   -- Actualiza al estado 'Pendiente de pago' cuando se asigna el recurso a una solicitud no pagada
   IF ((NEW."Status" = 'solicitud' OR NEW."Status" = 'alternativas') AND NEW."Resource_id" IS NOT NULL) THEN
     NEW."Status" :='pendientepago';
-    NEW."Expiry_date" := (CURRENT_DATE + INTERVAL '2 days');
-    change := 'Pendiente de pago';
   END IF;
   
   -- Actualiza al estado 'Confirmada' cuando se asigna el recurso a una solicitud pagada
   IF ((NEW."Status" = 'solicitudpagada' OR NEW."Status" = 'alternativaspagada') AND NEW."Resource_id" IS NOT NULL) THEN
     NEW."Status" := 'confirmada';
-    NEW."Expiry_date" = NULL;
-    change := 'Reserva confirmada';
+  END IF;
+
+  -- Actualiza al estado 'Checkin confirmado' cuando se asigna la fecha de checkin
+  IF (NEW."Status" = 'contrato'AND NEW."Check_in" IS NOT NULL) THEN
+    NEW."Status" := 'checkinconfirmado';
   END IF;
 
   -- Cambios de estado
   IF (NEW."Status" <> OLD."Status") THEN
 
+    -- Alternativas, envía mail
+    IF (OLD."Status" = 'solicitud' AND NEW."Status" = 'alternativa') OR
+       (OLD."Status" = 'solicitudpagada' AND NEW."Status" = 'alternativapagada') THEN
+      INSERT INTO "Customer"."Customer_email" ("Customer_id", "Template", "Entity_id") VALUES (NEW."Customer_id", 'alternativa', NEW.id);
+      change := 'Revisar alternativas';
+    END IF;
+
     -- Pendiente de pago, envía mail
-    IF (NEW."Status" = 'confirmada' ) THEN
+    IF (NEW."Status" = 'pendientepago' ) THEN
+
+      -- Actualiza la fecha de expiracion
+      IF NEW."Expiry_date" IS NULL
+        NEW."Expiry_date" := (CURRENT_DATE + INTERVAL '2 days');
+      ELSE
+        IF NEW."Expiry_date" < (CURRENT_DATE + INTERVAL '2 days') THEN
+          NEW."Expiry_date" := (CURRENT_DATE + INTERVAL '2 days');
+        END IF;
+      END IF;
+
+      -- Email
       INSERT INTO "Customer"."Customer_email" ("Customer_id", "Template", "Entity_id") VALUES (NEW."Customer_id", 'pendientepago', NEW.id);
+
+      -- Log
+      change := 'Pendiente de pago';
     END IF;
 
     -- Confirmada, inserta pago de garantía pendiente y envía mail
     IF (NEW."Status" = 'confirmada' ) THEN
+
+      -- Borra fecha expiración
+      NEW."Expiry_date" = NULL;
+
+      -- Depósito
       INSERT INTO "Billing"."Payment" ("Payment_method_id", "Booking_id", "Amount", "Issued_date", "Concept", "Payment_type" )  VALUES ('1', NEW.id, NEW."Deposit", CURRENT_DATE, 'Booking deposit', 'deposito');
+
+      -- EMail
       INSERT INTO "Customer"."Customer_email" ("Customer_id", "Template", "Entity_id") VALUES (NEW."Customer_id", 'confirmada', NEW.id);
+
+      -- Log
+      change := 'Reserva confirmada';
+
     END IF;
 
   END IF;
--- function/Booking.Booking_option_B1_accept_option.sql
+++ function/Booking.Booking_option_B1_accept_option.sql
@@ -6,7 +6,14 @@ BEGIN
   -- Update request with accepted option
   IF NEW."Accepted" THEN
     UPDATE "Booking"."Booking"
-    SET "Building_id" = NEW."Building_id", "Flat_type_id" = NEW."Flat_type_id", "Place_type_id" = NEW."Place_type_id"
+    SET "Building_id" = NEW."Building_id", 
+      "Flat_type_id" = NEW."Flat_type_id", 
+      "Place_type_id" = NEW."Place_type_id"
+      "Status" = CASE
+        WHEN "Status" = 'alternativas' THEN 'solicitud'
+        WHEN "Status" = 'alternativaspagadas' THEN 'solicitudpagada'
+        ELSE "Status"
+      END
     WHERE id = NEW."Booking_id";
   END IF;
 
