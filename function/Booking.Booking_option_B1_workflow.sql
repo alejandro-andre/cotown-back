f3e003ad88ad2f2c24e75b65631d342e89c0e4ed
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-18 13:53:05 +0200

    Fix

--- function/Booking.Booking_option_B1_workflow.sql
+++ function/Booking.Booking_option_B1_workflow.sql
@@ -36,15 +36,6 @@ BEGIN
     UPDATE "Booking"."Booking" SET "Status" = 'alternativaspagada' WHERE id = NEW."Booking_id";
   END IF;
 
-  -- Cancelacion de la solicitud confirmada
-  IF booking_status = 'cancelada' and THEN
-    SELECT COUNT(*) INTO deposit_paid FROM "Billing"."Payment" WHERE "Booking".id = NEW."Booking_id";
-    IF(deposit_paid >0) THEN
-        INSERT INTO "Billing"."Payment"("Payment_method_id", "Booking_id", "Amount", "Issued_date", "Concept", "Payment_type" ) VALUES ('1',NEW."id", NEW."Cancelation_fee", CURRENT_DATE, 'Booking cancel', 'penalización');
-        -- falta log
-    END IF;
-  END IF;
-
   -- Return
   RETURN NEW;
 
--- main.py
+++ main.py
@@ -162,7 +162,7 @@ def runapp():
             logger.debug('Provider found')
     
         # Create keycloak account
-        if create_user('P' + str(data['id']), data['Name'], data['Last_name'], data['Email'], 'P' + data['Document']):
+        if create_user('P' + str(data['id']), data['Name'], data['Last_name'], data['Email'], data['User_name']):
             logger.debug('Provider created')
 
         return
@@ -189,7 +189,7 @@ def runapp():
             logger.debug('Customer found')
     
         # Create keycloak account
-        if create_user('C' + str(data['id']), data['Name'], data['Last_name'], data['Email'], 'C' + data['Document']):
+        if create_user('C' + str(data['id']), data['Name'], data['Last_name'], data['Email'], data['User_name']):
             logger.debug('Customer created')
         return
 
