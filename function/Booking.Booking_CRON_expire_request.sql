769bfe2706c965f286b245be617a22edbd2261e3
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-21 11:43:30 +0200

    Documentacion

--- function/Booking.Booking_CRON_expire_request.sql
+++ function/Booking.Booking_CRON_expire_request.sql
@@ -1,4 +1,5 @@
+-- Caduca las solicitudes pendientes de pago pasadas de fecha
 BEGIN
-     RESET ROLE;
-     UPDATE "Booking"."Booking" SET "Status"='caducada' WHERE "Booking"."Expiry_date" < CURRENT_DATE AND "Booking"."Status"='pendientepago';
+  RESET ROLE;
+  UPDATE "Booking"."Booking" SET "Status"='caducada' WHERE "Booking"."Expiry_date" < CURRENT_DATE AND "Booking"."Status"='pendientepago';
 END;
