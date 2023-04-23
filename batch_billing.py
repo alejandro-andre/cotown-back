eca7cd337ad37907565e0946d445275b5a3107ff
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-23 16:38:27 +0200

    Bill payments

--- batch_billing.py
+++ batch_billing.py
@@ -10,6 +10,7 @@
 
 # System includes
 import os
+from datetime import datetime
 
 # Logging
 import logging
@@ -17,10 +18,95 @@ logger = logging.getLogger('COTOWN')
 
 # Cotown includes
 from library.services.apiclient import APIClient
+from library.services.dbclient import DBClient
 
 
 # ###################################################
-# Bill generator function
+# Generate booking fee and deposit payment bills
+# ###################################################
+
+def bill_payments(dbClient):
+
+    # Capture exceptions
+    try:
+
+        # Get all bookings without bill
+        dbClient.select('''
+            SELECT p.id, p."Payment_type", p."Customer_id", p."Booking_id", p."Payment_method_id", p."Amount", r."Owner_id" 
+            FROM "Billing"."Payment" p
+            INNER JOIN "Booking"."Booking" b ON p."Booking_id" = b.id
+            INNER JOIN "Resource"."Resource" r ON b."Resource_id" = r.id
+            LEFT JOIN "Billing"."Invoice" i ON i."Payment_id" = b.id
+            WHERE "Payment_date" IS NOT NULL
+            AND i.id IS NULL
+        ''')
+        data = dbClient.fetchall()
+
+        # Loop thru payments
+        for item in data:
+
+            # Debug
+            logger.debug(item)
+
+            # Create invoice
+            dbClient.execute('''
+                INSERT INTO "Billing"."Invoice" 
+                ("Bill_type", "Issued", "Issued_date", "Provider_id", "Customer_id", "Booking_id", "Payment_method_id", "Payment_id", "Details")
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                RETURNING id
+                ''', 
+                (
+                    'factura' if item['Payment_type'] == 'booking' else 'recibo', 
+                    True, 
+                    datetime.now(), 
+                    1 if item['Payment_type'] == 'booking' else item['Owner_id'], 
+                    item['Customer_id'], 
+                    item['Booking_id'], 
+                    item['Payment_method_id'], 
+                    item['id'], 
+                    'Booking fee' if item['Payment_type'] == 'booking' else 'Garantía', 
+                )
+            )
+            billid = dbClient.returning()[0]
+
+            # Create invoice line
+            dbClient.execute('''
+                INSERT INTO "Billing"."Invoice_line" 
+                ("Invoice_id", "Amount", "Product_id", "Tax_id")
+                VALUES (%s, %s, %s, %s)
+                ''', 
+                (
+                    billid, 
+                    item['Amount'], 
+                    1 if item['Payment_type'] == 'booking' else 2, 
+                    1 if item['Payment_type'] == 'booking' else 2, 
+                )
+            )
+
+        # End
+        dbClient.commit()
+        dbClient.disconnect()
+        return
+
+    # Process exception
+    except Exception as error:
+        logger.error(error)
+        dbClient.rollback()
+        dbClient.disconnect()
+        return
+
+
+# ###################################################
+# Generate monthly bills
+# ###################################################
+
+def bill_rent(dbClient):
+    
+    pass
+
+
+# ###################################################
+# Billing process
 # ###################################################
 
 def main():
@@ -43,34 +129,31 @@ def main():
     # ###################################################
 
     SERVER   = str(os.environ.get('COTOWN_SERVER'))
-    GQLUSER  = str(os.environ.get('COTOWN_GQLUSER'))
-    GQLPASS  = str(os.environ.get('COTOWN_GQLPASS'))
+    DATABASE = str(os.environ.get('COTOWN_DATABASE'))
+    DBUSER   = str(os.environ.get('COTOWN_DBUSER'))
+    DBPASS   = str(os.environ.get('COTOWN_DBPASS'))
+    SSHUSER  = str(os.environ.get('COTOWN_SSHUSER'))
+    SSHPASS  = str(os.environ.get('COTOWN_SSHPASS'))
 
 
     # ###################################################
-    # GraphQL and DB client
+    # DB client
     # ###################################################
 
-    # graphQL API
-    apiClient = APIClient(SERVER)
-    apiClient.auth(user=GQLUSER, password=GQLPASS)
+    # DB API
+    dbClient = DBClient(SERVER, DATABASE, DBUSER, DBPASS, SSHUSER, SSHPASS)
+    dbClient.connect()
 
 
     # ###################################################
     # Main
     # ###################################################
 
-    # 1. Generate invoice for each booking fee payment 
-    #    Get every booking fee payment without bill
-    pass
+    # 1. Generate invoice for each booking fee and deposit payment 
+    bill_payments(dbClient)
 
-    # 2. Generate recepit for each deposit payment 
-    #    Get every deposit payment without bill
-    pass
-
-    # 3. Monthly billing process
-    #    Process each inhouse booking
-    pass
+    # 2. Monthly billing process
+    bill_rent(dbClient)
 
 
 # #####################################
