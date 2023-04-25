51355cb781294a25c7fc069efdc3fe1e6327ea97
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-25 22:53:09 +0200

    Fix

--- function/Booking.Booking_group_B2_create_prices.sql
+++ function/Booking.Booking_group_B2_create_prices.sql
@@ -8,12 +8,17 @@ BEGIN
 
   RESET ROLE;
   
+  -- Delete old prices
+  DELETE FROM "Booking"."Booking_group_price"
+  WHERE "Booking_id" = NEW.id
+  AND ("Rent_date" < NEW."Date_from" OR "Rent_date" > NEW."Date_to" );
+
   -- Insert prices
   dt_curr = NEW."Date_from";
   dt_to = NEW."Date_to" + INTERVAL '1 day';
   WHILE dt_curr < dt_to LOOP
     BEGIN
-      INSERT INTO "Booking"."Booking_price" ("Booking_id", "Rent_date", "Rent", "Services") VALUES (NEW.id, dt_curr, 0, 0);
+      INSERT INTO "Booking"."Booking_group_price" ("Booking_id", "Rent_date", "Rent", "Services") VALUES (NEW.id, dt_curr, 0, 0);
     EXCEPTION WHEN unique_violation THEN
       NULL;
     END;
