02649b3810d24a240e8489e212cfc6fc9de8531e
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-25 09:33:57 +0200

    Validaciones adicionales

--- function/Booking.Booking_B2_validate.sql
+++ function/Booking.Booking_B2_validate.sql
@@ -8,39 +8,23 @@ DECLARE
 
 BEGIN
 
-  -- Payer
-  IF NEW."Payer_id" IS NULL THEN
-    NEW."Payer_id" = NEW."Customer_id";
-  END IF;
-
   -- Locked booking
-  IF NEW."Lock" AND OLD."Resource_id" IS NOT NULL THEN
-    NEW."Resource_id" := OLD."Resource_id";
+  IF NEW."Lock" AND 
+    OLD."Resource_id" IS NOT NULL AND
+    OLD."Resource_id" <> NEW."Resource_id" THEN
+    RAISE exception '!!!Locked booking!!!Reserva bloqueada!!!';
   END IF;
 
-  -- Request and request expiratuion date
-  IF NEW."Request_date" IS NULL THEN
-    NEW."Request_date" := NOW();
-    NEW."Expiry_date" := NOW() + INTERVAL '2 day';
+  -- Resource removed
+  IF NEW."Status" NOT IN ('solicitud', 'solicitudpagada', 'alternativas', 'alternativaspagada') AND
+    NEW."Resource_id" IS NULL THEN
+    RAISE exception '!!!Resource cannot be removed!!!El recurso no se puede quitar!!!';
   END IF;
 
   -- Check request dates
   IF NEW."Date_from" > NEW."Date_to" THEN
     RAISE exception '!!!Wrong dates!!!Fechas incorrectas!!!';
   END IF;
-
-  -- Validate duration
-  --SELECT AGE(NEW."Date_to" + INTERVAL '1 day', NEW."Date_from") INTO duration;
-  --SELECT EXTRACT(YEAR FROM duration) INTO years;
-  --SELECT EXTRACT(MONTH FROM duration) INTO months;
-  --SELECT EXTRACT(DAY FROM duration) INTO days;
-  --IF years > 0 OR months > 11 OR (months = 11 AND days > 0) THEN
-  --  RAISE exception '!!!The stay should be maximun 11 months long!!!La estancia debe ser como máximo de 11 meses!!!';
-  --ELSE
-  --  IF months < 1 THEN
-  --    RAISE exception '!!!The stay should be at least 1 month long!!!La estancia debe ser de un mes o superior!!!';
-  --  END IF;
-  --END IF;
 
   -- Validate checkin checkout dates
   IF NEW."Check_in" IS NOT NULL THEN
@@ -66,6 +50,29 @@ BEGIN
       RAISE EXCEPTION '!!!The expiry date must be equal to or greater than the date of application.!!!La fecha de expiración debe ser igual o mayor a la fecha de solicitud!!!';
     END IF;
   END IF;
+
+  -- Payer
+  IF NEW."Payer_id" IS NULL THEN
+    NEW."Payer_id" = NEW."Customer_id";
+  END IF;
+
+  -- Request date
+  IF NEW."Request_date" IS NULL THEN
+    NEW."Request_date" := NOW();
+  END IF;
+
+  -- Validate duration
+  --SELECT AGE(NEW."Date_to" + INTERVAL '1 day', NEW."Date_from") INTO duration;
+  --SELECT EXTRACT(YEAR FROM duration) INTO years;
+  --SELECT EXTRACT(MONTH FROM duration) INTO months;
+  --SELECT EXTRACT(DAY FROM duration) INTO days;
+  --IF years > 0 OR months > 11 OR (months = 11 AND days > 0) THEN
+  --  RAISE exception '!!!The stay should be maximun 11 months long!!!La estancia debe ser como máximo de 11 meses!!!';
+  --ELSE
+  --  IF months < 1 THEN
+  --    RAISE exception '!!!The stay should be at least 1 month long!!!La estancia debe ser de un mes o superior!!!';
+  --  END IF;
+  --END IF;
 
   -- Return record
   RETURN NEW;
