514af02f211439423c02814263d77d7d19968732
Alejandro André <alejandro.andre@ciberexperis.es>
2023-04-28 13:35:28 +0200

    Emails v0.1

--- library/business/send_email.py
+++ library/business/send_email.py
@@ -7,6 +7,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from jinja2 import Environment 
 import smtplib
+import markdown
 import ssl
 import logging
 
@@ -55,6 +56,7 @@ BASE = '''
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>{}</title>
 </head>
 <body  style="font-family: Arial, sans-serif; font-size: 16px;">{}</body>
 </html>
@@ -92,7 +94,8 @@ def do_email(apiClient, email):
 
   # Generate body
   text = template['Body']
-  body = BASE.format(env.from_string(text).render(context))
+  md = env.from_string(text).render(context)
+  body = BASE.format(subject, markdown.markdown(md, extensions=['tables', 'attr_list']))
 
   # Return
   return subject, body
