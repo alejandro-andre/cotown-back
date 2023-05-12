31e06bd70e67c657686caa9f12c1462b97b4fe7a
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-12 18:43:34 +0200

    Fix lock validation

--- function/Resource.Resource_availability_B1_validate.sql
+++ function/Resource.Resource_availability_B1_validate.sql
@@ -2,22 +2,26 @@
 -- BEFORE INSERT/UPDATE
 DECLARE
 
-	count INTEGER;
+  count INTEGER;
 
 BEGIN
 
+  -- Valida las fechas
   IF NEW."Date_to" <= NEW."Date_from" THEN
     RAISE EXCEPTION '!!!End date must be greater than start date.!!!La fecha final debe ser mayor o igual que la fecha de inicio.!!!';
   END IF;
  
-  SELECT COUNT(*)
-  INTO count
-  FROM "Booking"."Booking_detail" bd 
-  WHERE bd."Date_from" <= NEW."Date_to" 
-  AND bd."Date_to" >= NEW."Date_from";
-
+  -- Cuenta reservas en este piso en las mismas fechas
+  SELECT COUNT(*) 
+  INTO count 
+  FROM "Booking"."Booking_detail" bd
+  INNER JOIN "Resource"."Resource" r ON r.id = bd."Resource_id" 
+  WHERE "Date_from" <= NEW."Date_to" 
+  AND "Date_to" >= NEW."Date_from"
+  AND r."Flat_id" = NEW.id; 
+ 
   IF count > 0 THEN
-    RAISE EXCEPTION '!!!Lock overlaps with active bookings!!!El bloqueo se solapa con reservas activas!!!';
+    RAISE EXCEPTION '!!!Locks overlap with active bookings in this flat!!!Bloqueos se solapan con reservas activas en este piso!!!';
   END IF;
  
   RETURN NEW;
