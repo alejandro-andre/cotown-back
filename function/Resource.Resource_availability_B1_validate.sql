495483f3e39622ab3a16d1ea13a468b9c921aace
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-13 17:32:45 +0200

    Fix!!

--- function/Resource.Resource_availability_B1_validate.sql
+++ function/Resource.Resource_availability_B1_validate.sql
@@ -18,7 +18,7 @@ BEGIN
   INNER JOIN "Resource"."Resource" r ON r.id = bd."Resource_id" 
   WHERE "Date_from" <= NEW."Date_to" 
   AND "Date_to" >= NEW."Date_from"
-  AND r."Flat_id" = NEW.id; 
+  AND r."Flat_id" = NEW."Resource_id"; 
  
   IF count > 0 THEN
     RAISE EXCEPTION '!!!Locks overlap with active bookings in this flat!!!Bloqueos se solapan con reservas activas en este piso!!!';
