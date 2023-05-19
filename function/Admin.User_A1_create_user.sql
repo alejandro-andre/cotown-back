b4a099c87d4713672ede5900338c83a94c82969f
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-19 09:24:09 +0200

    Default role

--- function/Admin.User_A1_create_user.sql
+++ function/Admin.User_A1_create_user.sql
@@ -21,10 +21,17 @@ BEGIN
   -- Asigna el usuario
   UPDATE "Admin"."User" SET "User_name" = user_name WHERE id = NEW.id;
 
-  -- Crea el rol en Postgres
+  -- Crea el usuario en Postgres
   IF NOT EXISTS (SELECT * FROM pg_roles WHERE rolname = user_name) THEN
     EXECUTE 'CREATE ROLE "' || user_name || '" PASSWORD ''UNK0WN_P4$$W0RD'' NOSUPERUSER';
   END IF;
+
+  -- Inserta el rol base (cotown) en Airflows
+  INSERT INTO "Models"."UserRole" ("user", "role") VALUES (user_id, 100)
+  ON CONFLICT ("user", "role") DO NOTHING;
+
+  -- Asigna el rol en Postgres
+  EXECUTE 'GRANT "cotown" TO "' || user_name || '"';
 
   -- Fin
   RETURN NEW;
