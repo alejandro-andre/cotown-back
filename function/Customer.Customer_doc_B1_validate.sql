9114a8cd3998c2b6cf705a2cc5336f3f808247f9
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-29 11:32:19 +0200

    Fix

--- function/Customer.Customer_doc_B1_validate.sql
+++ function/Customer.Customer_doc_B1_validate.sql
@@ -10,12 +10,12 @@ BEGIN
   INTO num
   FROM "Customer"."Customer_doc_type" cd
   WHERE id = NEW."Customer_doc_type_id";
-  IF NEW."Document" IS NULL THEN
-    RAISE EXCEPTION '!!!Please, attach the document!!!Por favor, adjunta el documento!!!';
-  END IF;
-  IF num > 1 AND NEW."Document_back" IS NULL THEN
-    RAISE EXCEPTION '!!!Please, attach two images for this document!!!Por favor, adjunta dos imagenes en este documento!!!';
-  END IF;
+  --?IF NEW."Document" IS NULL THEN
+  --?  RAISE EXCEPTION '!!!Please, attach the document!!!Por favor, adjunta el documento!!!';
+  --?END IF;
+  --?IF num > 1 AND NEW."Document_back" IS NULL THEN
+  --?  RAISE EXCEPTION '!!!Please, attach two images for this document!!!Por favor, adjunta dos imagenes en este documento!!!';
+  --?END IF;
 
   -- Validates that the expiry date of a document is greater than the current date. validateCustomerDocDateExpiryDate
   IF NEW."Expiry_date" IS NOT NULL THEN
