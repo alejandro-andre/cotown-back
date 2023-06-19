aea048063551a462f4c6014820fca3d2fc983cfc
Alejandro André <alejandro.andre@ciberexperis.es>
2023-06-19 17:53:32 +0200

    Upload fix

--- upload/upload.py
+++ upload/upload.py
@@ -1,18 +1,29 @@
+# ##################################################
+# Imports
+# ##################################################
+
 from PIL import Image
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 from io import BytesIO
-
 import base64
 import mimetypes
 import requests
-import json
 import os
 
+
+# ##################################################
+# Constants
+# ##################################################
 
 SERVER = 'experis.flows.ninja'
 USER   = 'modelsadmin'
 PASS   = 'Ciber$2022'
+
+
+# ##################################################
+# Upload files to Airflows
+# ##################################################
 
 def upload(folder):
 
@@ -23,65 +34,83 @@ def upload(folder):
 
   # Upload all files
   for root, dirs, files in os.walk(folder):
-    for file in files:
 
-      # Prepare upload URL
-      path = root + '\\' + file
-      schema = root.split('\\')[1].split('.')
-      name = file.split('_')
-      id = name[0]
+    # Folders
+    for dir in dirs:
 
-      # Guess mimetype
-      mimetype, _ = mimetypes.guess_type(path)
+      # Schema, entity and field
+      schema, entity, field = dir.split('.')
 
-      # Image?
-      if 'image/' in mimetype:
-
-        # Generate thumbnail
-        image = Image.open(path)
-        width, height = image.size
-        image.thumbnail((256 , 256))
-        thumbnail_data = BytesIO()
-        image.save(thumbnail_data, format='PNG')
-        thumbnail_data.seek(0)
-        thumbnail_base64 = base64.b64encode(thumbnail_data.read()).decode('utf-8')
-
-      # Upload file
-      print('Uploading ' + file)
-      with open(path, "rb") as f:
-        url = 'https://' + SERVER + '/document/' \
-            + schema[0] + '/' + schema[1] + '/' + id + '/' + schema[2] \
-            + '/contents?access_token=' + token
-        response = requests.post(url, data=f.read(), headers={ 'Content-Type': mimetype })
-        oid = int(response.content)
-
-      # Update graphQL
-      query = 'mutation ($id: Int! $file:Models_DocumentTypeInputType) { data: ' \
-            + schema[0] + '_' + schema[1] \
-            + 'Update ( where: { id : { EQ: $id } } entity: { ' \
-            + schema[2] +': $file } ) { id } }'
-      
-      # Update variables
-      vars = {
-        'authorization': token,
-        'id': int(id),
-        'file': { 
-          'oid': oid, 
-          'type': mimetype,
-          'name': name[1] 
-        }
-      }
-
-      # Image?
-      if 'image/' in mimetype:
-        vars['file']['thumbnail'] = 'data:' + mimetype + ';base64,' + thumbnail_base64
-        vars['file']['width'] = width
-        vars['file']['height'] = height
-
-      # Update
-      print('Updating ' + file)
+      # graphQL query
+      query = '{data:' + schema + '_' + entity + 'List{id ' + field + '{name}}}'
+      vars = { 'authorization': token }
       result = client.execute(gql(query), vars)
 
+      # Process each record
+      for item in result['data']:
+        if item[field]:
+
+          # File info
+          id = item['id'] 
+          name = item[field]['name'] 
+          path = root + '/' + dir + '/' + name
+
+          # Guess mimetype
+          mimetype, _ = mimetypes.guess_type(path)
+
+          # Image?
+          if 'image/' in mimetype:
+
+            # Generate thumbnail
+            image = Image.open(path)
+            width, height = image.size
+            image.thumbnail((256 , 256))
+            data = BytesIO()
+            image.save(data, format='PNG')
+            data.seek(0)
+            thumbnail = base64.b64encode(data.read()).decode('utf-8')
+
+          # Upload file
+          print('Uploading ' + name)
+          with open(path, "rb") as f:
+            url = 'https://' + SERVER + '/document/' \
+                + schema + '/' + entity + '/' + str(id) + '/' + field \
+                + '/contents?access_token=' + token
+            response = requests.post(url, data=f.read(), headers={ 'Content-Type': mimetype })
+            oid = int(response.content)
+
+          # Update graphQL
+          query = 'mutation ($id: Int! $file:Models_DocumentTypeInputType) { data: ' \
+                + schema + '_' + entity \
+                + 'Update ( where: { id : { EQ: $id } } entity: { ' \
+                + field +': $file } ) { id } }'
+          
+          # Update variables
+          vars = {
+            'authorization': token,
+            'id': int(id),
+            'file': { 
+              'oid': oid, 
+              'type': mimetype,
+              'name': name[1] 
+            }
+          }
+
+          # Image?
+          if 'image/' in mimetype:
+            vars['file']['thumbnail'] = 'data:' + mimetype + ';base64,' + thumbnail
+            vars['file']['width'] = width
+            vars['file']['height'] = height
+
+          # Update
+          print('Updating ' + name)
+          result = client.execute(gql(query), vars)
+
+
+# ##################################################
+# Main
+# ##################################################
 
 # Directorio raíz que se desea recorrer
-upload('files')
+if __name__ == '__main__':
+  upload('files')
