c504339ade463fd7a94019f17a3c11fc9bdae0e7
Alejandro André <alejandro.andre@ciberexperis.es>
2023-06-24 21:11:53 +0200

    Prices %

---
+++ function/Booking.Booking_price_B1_validate.sql
@@ -0,0 +1,27 @@
+-- Precios y descuentos
+BEGIN
+
+  -- Rent discount
+  IF NEW."Rent_discount_pct" IS NOT NULL THEN 
+  	NEW."Rent_discount" := NEW."Rent" * NEW."Rent_discount_pct" / 100;
+  --ELSE 
+  --  IF NEW."Rent_discount" IS NOT NULL THEN
+  --    NEW."Rent_discount_pct" := NEW."Rent_discount" / NEW."Rent" * 100;
+  --    NEW."Rent_discount" := NEW."Rent" * NEW."Rent_discount_pct" / 100;
+  --  END IF;
+  END IF;
+ 
+  -- Services discount
+  IF NEW."Services_discount_pct" IS NOT NULL THEN 
+  	NEW."Services_discount" := NEW."Services" * NEW."Services_discount_pct" / 100;
+  --ELSE 
+  --  IF NEW."Services_discount" IS NOT NULL THEN
+  --    NEW."Services_discount_pct" := NEW."Services_discount" / NEW."Services" * 100;
+  --    NEW."Services_discount" := NEW."Services" * NEW."Services_discount_pct" / 100;
+  --  END IF;
+  END IF;
+ 
+  -- Return record
+  RETURN NEW;
+
+END
