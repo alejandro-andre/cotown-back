688f78ced42d819121d2c9981d4394015a25f45d
Alejandro André <alejandro.andre@ciberexperis.es>
2023-06-26 19:17:20 +0200

    Validate option

---
+++ function/Booking.Booking_option_B1_validate.sql
@@ -0,0 +1,9 @@
+-- Verifica los datos de la opcion
+BEGIN
+	
+  -- Tipología obligatoria
+  IF NEW."Resource_type" <> 'piso' AND NEW."Place_type_id" IS NULL THEN
+    RAISE exception '!!!Place type is mandatory!!!El tipo de plaza es obligatoria!!!'; 	
+  END IF;
+
+END;
