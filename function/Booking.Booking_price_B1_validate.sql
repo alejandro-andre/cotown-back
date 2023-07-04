27c05d4c3fd815d030dd34e21379e46106befe23
Alejandro André <alejandro.andre@ciberexperis.es>
2023-07-04 18:00:14 +0200

    Fix

--- function/Booking.Booking_price_B1_validate.sql
+++ function/Booking.Booking_price_B1_validate.sql
@@ -3,21 +3,21 @@ BEGIN
 
   -- Rent discount
   IF NEW."Rent_discount" IS NOT NULL THEN
-    NEW."Rent_discount_pct" := NEW."Rent_discount" / NEW."Rent" * 100;
+    NEW."Rent_discount_pct" := (NEW."Rent_discount" * 100) / NEW."Rent";
   ELSE
     IF NEW."Rent_discount_pct" IS NOT NULL THEN 
     	NEW."Rent_discount" := NEW."Rent" * NEW."Rent_discount_pct" / 100;
-      NEW."Rent_discount_pct" := NEW."Rent_discount" / NEW."Rent" * 100;
+      NEW."Rent_discount_pct" := (NEW."Rent_discount" * 100) / NEW."Rent";
     END IF;
   END IF;
  
   -- Services discount
   IF NEW."Services_discount" IS NOT NULL THEN
-    NEW."Services_discount_pct" := NEW."Services_discount" / NEW."Services" * 100;
+    NEW."Services_discount_pct" := (NEW."Services_discount" * 100) / NEW."Services";
   ELSE
     IF NEW."Services_discount_pct" IS NOT NULL THEN 
     	NEW."Services_discount" := NEW."Services" * NEW."Services_discount_pct" / 100;
-      NEW."Services_discount_pct" := NEW."Services_discount" / NEW."Services" * 100;
+      NEW."Services_discount_pct" := (NEW."Services_discount" * 100) / NEW."Services";
     END IF;
   END IF;
  
