3dbd08629b013c6670c563d32c960326060a79a1
Alejandro André <alejandro.andre@ciberexperis.es>
2023-09-01 09:24:47 +0200

    Fix permissions

--- function/Provider.Provider_B2_validate.sql
+++ function/Provider.Provider_B2_validate.sql
@@ -1,17 +1,27 @@
 -- Verifica el proveedor
-DECLARE 
+DECLARE
 
+  curr_user VARCHAR;
   num INTEGER;
   
 BEGIN
 
   -- Cambio de email
   IF (OLD."Email" IS NOT NULL AND OLD."Email" <> NEW."Email") THEN
+
+    -- Superuser ROLE
+    curr_user := CURRENT_USER;
+    RESET ROLE;  
   	SELECT COUNT(*) INTO num FROM "Models"."User" WHERE email = NEW."Email";
+
     IF num > 0 THEN
       RAISE EXCEPTION '!!!Email already exists!!!El email ya existe!!!';
     END IF;
-    UPDATE "Models"."User" SET email = NEW."Email" WHERE username = NEW."User_name";
+    UPDATE "Models"."User" SET email = NEW."Email", password = 'Passw0rd!' WHERE username = NEW."User_name";
+
+    -- Fin
+    EXECUTE 'SET ROLE "' || curr_user || '"';
+
   END IF;
 
   -- Ok
