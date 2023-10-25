81ab8a3f4520fdfbc68cad7e479e7533313152fc
Alejandro André <alejandro.andre@ciberexperis.es>
2023-10-25 17:01:35 +0200

    Booking fee

--- function/Booking.Booking_A1_start_workflow.sql
+++ function/Booking.Booking_A1_start_workflow.sql
@@ -20,11 +20,13 @@ BEGIN
   UPDATE "Booking"."Booking" SET "Status" = 'solicitud' WHERE id = NEW.id;
 
   -- Crea un pago con el booking fee
-  SELECT "Payment_method_id" INTO payment_method_id FROM "Customer"."Customer" WHERE id = NEW."Payer_id";
-  INSERT 
-    INTO "Billing"."Payment"("Payment_method_id", "Customer_id", "Booking_id", "Amount", "Issued_date", "Concept", "Payment_type" ) 
-    VALUES (COALESCE(payment_method_id, 1), NEW."Payer_id", NEW.id, NEW."Booking_fee", CURRENT_DATE, 'Booking fee', 'booking');
-
+  IF NEW."Booking_fee" > 0 THEN
+    SELECT "Payment_method_id" INTO payment_method_id FROM "Customer"."Customer" WHERE id = NEW."Payer_id";
+    INSERT 
+      INTO "Billing"."Payment"("Payment_method_id", "Customer_id", "Booking_id", "Amount", "Issued_date", "Concept", "Payment_type" ) 
+      VALUES (COALESCE(payment_method_id, 1), NEW."Payer_id", NEW.id, NEW."Booking_fee", CURRENT_DATE, 'Booking fee', 'booking');
+  END IF;
+  
   -- Log
   INSERT INTO "Booking"."Booking_log"("Booking_id", "Log") VALUES (NEW.id, 'Solicitud de recurso');
 
--- function/Booking.Booking_B1_init.sql
+++ function/Booking.Booking_B1_init.sql
@@ -2,6 +2,7 @@
 DECLARE
 
   booking_fee_amount INTEGER;
+  year INTEGER;
 
 BEGIN
 
@@ -10,13 +11,23 @@ BEGIN
     NEW."Status" := 'solicitud';
   END IF;
 
+  -- Year
+  year := EXTRACT(YEAR FROM NOW());
+  IF EXTRACT(MONTH FROM NOW()) > 8 THEN
+    year := year + 1;
+  END IF;  
+
   -- Obtiene el valor del booking fee
-  IF NEW."Resource_type" = 'piso' THEN
-    SELECT "Booking_fee_flat" INTO booking_fee_amount FROM "Building"."Building" WHERE id = NEW."Building_id";
-  ELSE
-    SELECT "Booking_fee" INTO booking_fee_amount FROM "Building"."Building" WHERE id = NEW."Building_id";
-  END IF;
-  NEW."Booking_fee" := booking_fee_amount;
+  SELECT "Booking_fee" 
+  INTO booking_fee_amount
+  FROM "Billing"."Pricing_detail" pd 
+  WHERE pd."Building_id" = NEW."Building_id"
+  AND pd."Flat_type_id" = NEW."Flat_type_id"
+  AND (pd."Place_type_id" = NEW."Place_type_id" OR NEW."Place_type_id" IS NULL)
+  AND pd."Year" = year
+  LIMIT 1;
+
+  NEW."Booking_fee" := COALESCE(booking_fee_amount, 0);
 
   -- Return
   RETURN NEW;
