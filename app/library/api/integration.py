0f9e8d0587074b1259bfda13d52bd84dc12ed7ad
Alejandro André <alejandro.andre@ciberexperis.es>
2023-12-11 12:44:23 +0100

    SAP Invoice API

--- app/library/api/integration.py
+++ app/library/api/integration.py
@@ -16,7 +16,7 @@ from datetime import datetime
 from library.services.config import settings
 
 # Cotown includes - business functions
-from library.business.integration import q_int_clients
+from library.business.integration import q_int_customers, q_int_invoices
 
 # Logging
 import logging
@@ -35,6 +35,8 @@ def req_pub_int_customers():
   '''
   Retrieve newly created or updated customers
   ---
+  tags:
+    - name: "Customers"
   parameters:
     - name: date
       in: query
@@ -135,4 +137,118 @@ def req_pub_int_customers():
       abort(400, 'Invalid date')
 
   # Return
-  return q_int_clients(g.dbClient, date + ' 00:00:00')
+  return q_int_customers(g.dbClient, date + ' 00:00:00')
+
+# ---------------------------------------------------
+# Invoices - Get recent invoices
+# ---------------------------------------------------
+
+def req_pub_int_invoices():
+  '''
+  Retrieve newly issued invoices
+  ---
+  tags:
+    - name: "Invoices"
+  parameters:
+    - name: date
+      in: query
+      type: string
+      required: false
+      default: "2023-01-01"
+      description: "Issued minimum date, YYYY-MM-DD format"
+    - name: Api-Key
+      in: header
+      type: string
+      required: true
+      description: "Security API KEY that must be present in the HTTP request header"
+  definitions:
+    Invoice:
+      type: object
+      properties:
+        document_type:
+          type: string
+          description: "Type of document"
+          enum:
+            - CI
+            - CCM
+        issuer_id:
+          type: string
+          description: "SAP Id of the company that issued the invoice"
+        invoice_id:
+          type: string
+          description: "Id of the invoice"
+        customer_id:
+          type: integer
+          format: int64        
+          description: "SAP Id of the billed customer"
+        currency:
+          type: string
+          description: "Currency of the invoice amounts, always 'EUR'"
+        date:
+          type: string
+          description: "Invoice issued date"
+        note:
+          type: string
+          description: "Comments"
+        lines:
+          type: array
+          description: "Invoice lines"
+          items:
+            $ref: '#/definitions/Invoice_line'
+    Invoice_line:
+      type: object
+      properties:
+        service_id:
+          type: string
+          description: "SAP Id of the billed service"
+        description:
+          type: string
+          description: "Description of the invoice content"
+        project_id:
+          type: string
+          description: "SAP Id of the project/task to allocate the amount"
+        amount:
+          type: float
+          description: "Amount of the billed service"
+        tax_id:
+          type: string
+          description: "SAP Id of the tax applied"
+        comments:
+          type: string
+          description: "Comments of the invoice line"
+  responses:
+    200:
+      description: List of issued invoices since the given date
+      content:
+        application/json:
+          schema:
+            type: array
+            items:
+              $ref: '#/definitions/Invoice'
+    400:
+      description: Invalid date or date format
+    403:
+      description: Invalid Api-Key
+  '''
+
+  # Debug
+  logger.debug('Integration - Invoices')
+
+  # Get API key
+  key = request.headers.get('Api-Key', None)
+  if key != settings.SAP_API_KEY:
+    logger.info('Invalid Api-Key: ' + str(key))
+    abort(403, 'Invalid Api-Key')
+
+  # Validate date
+  date = '2024-01-01'
+  d = request.args.get('date')
+  if d != None:
+    try:
+      date = datetime.strptime(str(d), '%Y-%m-%d').strftime('%Y-%m-%d')
+    except ValueError:
+      logger.info('Invalid date: ' + str(d))
+      abort(400, 'Invalid date')
+
+  # Return
+  return q_int_invoices(g.dbClient, date + ' 00:00:00')
--- app/library/business/integration.py
+++ app/library/business/integration.py
@@ -15,7 +15,7 @@ logger = logging.getLogger('COTOWN')
 # Client query
 # ------------------------------------------------------
 
-def q_int_clients(dbClient, date):
+def q_int_customers(dbClient, date):
 
   sql = f'''
     SELECT DISTINCT * FROM (
@@ -97,6 +97,78 @@ def q_int_clients(dbClient, date):
     cur = dbClient.execute(con, sql)
     column_names = [desc[0] for desc in cur.description]
     result = [{col: (row[i] if row[i] is not None else '') for i, col in enumerate(column_names)} for row in cur.fetchall()]
+    cur.close()
+    dbClient.putconn(con)
+    return result
+ 
+  except Exception as error:
+    logger.error(error)
+    con.rollback()
+    return None
+  
+# ------------------------------------------------------
+# Invoices query
+# ------------------------------------------------------
+
+def q_int_invoices(dbClient, date):
+
+  sql = f'''
+    SELECT
+      CASE 
+        WHEN i."Bill_type" = 'factura' THEN 'CI'
+        ELSE 'CCM'
+      END AS "document_type",
+      p."SAP_code" AS "issuer_id",
+      i."Code" AS "invoice_id",
+      c.id AS "customer_id",
+      'EUR' AS "currency",
+      TO_CHAR(i."Issued_date", 'DD/MM/YYYY') AS "date",
+      i."Comments" AS "comments",
+      pr."SAP_code" AS "service_id",
+      il."Concept" AS "description",
+      SUBSTRING(p."SAP_code", 1, 2) || r."SAP_code" AS "project_id",
+      il."Amount" AS "amount",
+      t."SAP_code" AS "tax_id", 
+      il."Comments" AS "comments"
+    FROM "Billing"."Invoice_line" il
+    INNER JOIN "Billing"."Invoice" i ON i.id = il."Invoice_id"
+    INNER JOIN "Billing"."Product" pr ON pr.id = il."Product_id"
+    INNER JOIN "Billing"."Tax" t ON t.id = il."Tax_id" 
+    INNER JOIN "Provider"."Provider" p ON p.id = i."Provider_id"
+    INNER JOIN "Customer"."Customer" c ON c.id = i."Customer_id"
+    INNER JOIN "Resource"."Resource" r ON r.id = il."Resource_id" 
+    WHERE i."Issued"
+      AND i."Bill_type" <> 'recibo'
+      AND p."SAP_code" IS NOT NULL
+      AND i."Issued_date" >= '{date}'
+    ORDER BY 2, 3
+  '''
+  try:
+    con = dbClient.getconn()
+    cur = dbClient.execute(con, sql)
+    result = []
+    for row in cur.fetchall():
+      invoice_index = next((index for (index, d) in enumerate(result) if d['issuer_id'] == row['issuer_id'] and d['invoice_id'] == row['invoice_id']), None)
+      if invoice_index is None:
+        result.append({
+          'comments': row['comments'],
+          'currency': row['currency'],
+          'customer_id': row['customer_id'],
+          'date': row['date'],
+          'document_type': row['document_type'],
+          'invoice_id': row['invoice_id'],
+          'issuer_id': row['issuer_id'],
+          'lines': []
+        })
+        invoice_index = len(result) - 1
+      result[invoice_index]['lines'].append({
+        'amount': row['amount'],
+        'comments': row['comments'],
+        'description': row['description'],
+        'project_id': row['project_id'],
+        'service_id': row['service_id'],
+        'tax_id': row['tax_id']
+      })
     cur.close()
     dbClient.putconn(con)
     return result
