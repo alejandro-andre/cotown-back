6dd35cf674546b3e5ed9a21658442e0e233398a1
alejandro.andre <alejandro.andre@experis.es>
2024-02-11 00:15:49 +0100

    Fix Tax & Management fee

--- app/templates/report/ingresos.ingresos.sql
+++ app/templates/report/ingresos.ingresos.sql
@@ -3,6 +3,7 @@
 SELECT pr."Name" as "Owner", EXTRACT(MONTH from i."Issued_date") AS "Month", EXTRACT(YEAR from i."Issued_date") AS "Year",
   i."Booking_id", b."Date_from", b."Date_to", r."Code", c."Name",
   il."Amount" AS "Amount",
+  t."Value" / 100 AS "Tax",
   CASE 
     WHEN p."Payment_date" IS NULL THEN il."Amount"
     ELSE 0.0
@@ -13,6 +14,7 @@ SELECT pr."Name" as "Owner", EXTRACT(MONTH from i."Issued_date") AS "Month", EXT
   pdt."Name" AS "Amount_type",
   CASE WHEN pr."Provider_type_id" = 1 THEN bu."Management_fee" / 100 ELSE NULL END AS "Management_fee"
 FROM "Billing"."Invoice_line" il
+  INNER JOIN "Billing"."Tax" t ON t.id = il."Tax_id"
   INNER JOIN "Billing"."Invoice" i on i.id = il."Invoice_id"  
   INNER JOIN "Billing"."Product" pd on pd.id = il."Product_id" 
   INNER JOIN "Billing"."Product_type" pdt on pdt.id = pd."Product_type_id" 
@@ -32,6 +34,7 @@ UNION ALL
 SELECT pr."Name" as "Owner", EXTRACT(MONTH from i."Issued_date") AS "Month", EXTRACT(YEAR from i."Issued_date") AS "Year",
   i."Booking_group_id", b."Date_from", b."Date_to", bu."Code"||' ('||b."Rooms"||' plazas)' AS "Code", c."Name",
   il."Amount" AS "Amount",
+  t."Value" / 100 AS "Tax",
   CASE 
     WHEN p."Payment_date" IS NULL THEN il."Amount"
     ELSE 0.0
@@ -41,6 +44,7 @@ SELECT pr."Name" as "Owner", EXTRACT(MONTH from i."Issued_date") AS "Month", EXT
   'B2B' AS "Type", pdt."Name" AS "Amount_type",
   CASE WHEN pr."Provider_type_id" = 1 THEN bu."Management_fee" / 100 ELSE NULL END AS "Management_fee"
 FROM "Billing"."Invoice_line" il
+  INNER JOIN "Billing"."Tax" t ON t.id = il."Tax_id"
   INNER JOIN "Billing"."Invoice" i on i.id = il."Invoice_id"  
   INNER JOIN "Billing"."Product" pd on pd.id = il."Product_id" 
   INNER JOIN "Billing"."Product_type" pdt on pdt.id = pd."Product_type_id" 
@@ -63,6 +67,7 @@ SELECT
     WHEN r."Owner_id" = r."Service_id" THEN bp."Rent" + COALESCE(bp."Rent_discount", 0) + bp."Services" + COALESCE(bp."Services_discount", 0)
     ELSE bp."Rent" + COALESCE(bp."Rent_discount", 0)
   END AS "Amount",
+  t."Value" / 100 AS "Tax",
   CASE 
     WHEN r."Owner_id" = r."Service_id" THEN bp."Rent" + COALESCE(bp."Rent_discount", 0) + bp."Services" + COALESCE(bp."Services_discount", 0)
     ELSE bp."Rent" + COALESCE(bp."Rent_discount", 0)
@@ -73,7 +78,9 @@ SELECT
 FROM "Booking"."Booking_price" bp 
   INNER JOIN "Booking"."Booking" b on b.id = bp."Booking_id" 
   INNER JOIN "Resource"."Resource" r on r.id = b."Resource_id"  
-  INNER JOIN "Building"."Building" bu on bu.id = r."Building_id"  
+  INNER JOIN "Building"."Building" bu on bu.id = r."Building_id"
+  INNER JOIN "Building"."Building_type" bt ON bt.id = bu."Building_type_id"
+  INNER JOIN "Billing"."Tax" t ON t.id = bt."Tax_id"
   INNER JOIN "Provider"."Provider" pr on pr.id = r."Owner_id"  
   INNER JOIN "Customer"."Customer" c on c.id = b."Customer_id"
   INNER JOIN "Billing"."Product_type" pdt on pdt.id = 3
@@ -91,6 +98,7 @@ SELECT DISTINCT ON (bp.id)
     WHEN r."Owner_id" = r."Service_id" THEN b."Rooms" * (bp."Rent" + bp."Services")
     ELSE b."Rooms" * bp."Rent"
   END AS "Amount",
+  t."Value" / 100 AS "Tax",
   CASE 
     WHEN r."Owner_id" = r."Service_id" THEN b."Rooms" * (bp."Rent" + bp."Services")
     ELSE b."Rooms" * bp."Rent"
@@ -102,6 +110,8 @@ FROM "Booking"."Booking_group_price" bp
   INNER JOIN "Booking"."Booking_group" b on b.id = bp."Booking_id" 
   INNER JOIN "Booking"."Booking_group_rooming" br on b.id = br."Booking_id" 
   INNER JOIN "Building"."Building" bu on bu.id = b."Building_id" 
+  INNER JOIN "Building"."Building_type" bt ON bt.id = bu."Building_type_id"
+  INNER JOIN "Billing"."Tax" t ON t.id = bt."Tax_id"
   INNER JOIN "Resource"."Resource" r on r.id = br."Resource_id"  
   INNER JOIN "Provider"."Provider" pr on pr.id = r."Owner_id"  
   INNER JOIN "Customer"."Customer" c on c.id = b."Payer_id"
@@ -119,6 +129,7 @@ SELECT
     WHEN r."Owner_id" = r."Service_id" THEN 0
     ELSE bp."Services" + COALESCE(bp."Services_discount", 0)
   END AS "Amount",
+  t."Value" / 100 AS "Tax",
   CASE 
     WHEN r."Owner_id" = r."Service_id" THEN 0
     ELSE bp."Services" + COALESCE(bp."Services_discount", 0)
@@ -127,6 +138,7 @@ SELECT
   'B2C' AS "Type", pdt."Name" AS "Amount_type",
   CASE WHEN pr."Provider_type_id" = 1 THEN bu."Management_fee" / 100 ELSE NULL END AS "Management_fee"
 FROM "Booking"."Booking_price" bp 
+  INNER JOIN "Billing"."Tax" t ON t.id = 1
   INNER JOIN "Booking"."Booking" b on b.id = bp."Booking_id" 
   INNER JOIN "Resource"."Resource" r on r.id = b."Resource_id"  
   INNER JOIN "Building"."Building" bu on bu.id = r."Building_id"  
@@ -146,6 +158,7 @@ SELECT DISTINCT ON (bp.id)
     WHEN r."Owner_id" = r."Service_id" THEN 0
     ELSE b."Rooms" * bp."Services"
   END AS "Amount",
+  t."Value" / 100 AS "Tax",
   CASE 
     WHEN r."Owner_id" = r."Service_id" THEN 0
     ELSE b."Rooms" * bp."Services"
@@ -154,6 +167,7 @@ SELECT DISTINCT ON (bp.id)
   'B2B' AS "Type", pdt."Name" AS "Amount_type",
   CASE WHEN pr."Provider_type_id" = 1 THEN bu."Management_fee" / 100 ELSE NULL END AS "Management_fee"
 FROM "Booking"."Booking_group_price" bp 
+  INNER JOIN "Billing"."Tax" t ON t.id = 1
   INNER JOIN "Booking"."Booking_group" b on b.id = bp."Booking_id" 
   INNER JOIN "Booking"."Booking_group_rooming" br on b.id = br."Booking_id" 
   INNER JOIN "Building"."Building" bu on bu.id = b."Building_id" 
@@ -163,5 +177,7 @@ FROM "Booking"."Booking_group_price" bp
   INNER JOIN "Billing"."Product_type" pdt on pdt.id = 4
 WHERE bp."Invoice_rent_id" IS NULL 
   AND b."Status" IN ('grupoconfirmado', 'inhouse') 
-  AND bp."Rent_date" >= %(fdesde)s AND bp."Rent_date" < %(fhasta)s AND r."Owner_id" BETWEEN %(pdesde)s AND %(phasta)s)
+  AND bp."Rent_date" >= %(fdesde)s AND bp."Rent_date" < %(fhasta)s AND r."Owner_id" BETWEEN %(pdesde)s AND %(phasta)s
+)
 ORDER BY "Owner", "Month", "Year", "Code"
+;
