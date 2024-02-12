f8c2bcde5b17f55b5e2c45579e4d08e1dae4a416
alejandro.andre <alejandro.andre@experis.es>
2024-02-12 12:55:30 +0100

    ActiveCampaign

---
+++ app/templates/report/ac.ac.sql
@@ -0,0 +1,63 @@
+SELECT 
+	b.id,
+	c."Name",
+	c."Email",
+	c."Phones",
+	c."Birth_date",
+	EXTRACT(YEAR FROM age(c."Birth_date")) AS "Age",
+	g."Name" AS "Gender",
+	co."Name" AS "Nationality",
+	s."Name" AS "School",
+	c."Lang" AS "Language",
+	b."Request_date",
+	b."Confirmation_date",
+	bc."Name" AS "Origin",
+	br."Name" AS "Suborigin",
+	b."Date_from",
+	b."Date_to",
+	b."Check_in",
+	b."Check_out",
+	l."Name" AS "City",
+	substring(r."Code",1, 6) AS "Building",
+	substring(r."Code",8, 5) AS "Flat",
+	substring(r."Code",14, 9) AS "Place",
+	b."Rent",
+	b."Services",
+	b."Booking_fee",
+	sums."Total_rent",
+	sums."Total_services",
+	sums."Management_fee",
+	see."Name" AS "End_brand",
+	seo."Name" AS "Origin_brand",
+	'B2C' AS "Lead_type",
+	b."Status"
+FROM "Booking"."Booking" b
+  INNER JOIN "Booking"."Booking_channel" bc ON bc.id = b."Booking_channel_id" 
+  INNER JOIN "Booking"."Booking_referral" br ON br.id = b."Booking_referral_id" 
+  INNER JOIN "Resource"."Resource" r ON r.id = b."Resource_id" 
+  INNER JOIN "Building"."Building" bub ON bub.id = r."Building_id" 
+  INNER JOIN "Building"."Building" bur ON bur.id = b."Building_id" 
+  INNER JOIN "Geo"."District" d ON d.id = bub."District_id" 
+  INNER JOIN "Geo"."Location" l ON l.id = d."Location_id" 
+  INNER JOIN "Customer"."Customer" c ON c.id = b."Customer_id" 
+  INNER JOIN "Auxiliar"."Gender" g ON g.id = c."Gender_id"
+  INNER JOIN "Geo"."Country" co ON co.id = c."Nationality_id"
+  INNER JOIN "Auxiliar"."School" s ON s.id = b."School_id"
+  INNER JOIN "Auxiliar"."Segment" see ON see.id = bub."Segment_id"  
+  INNER JOIN "Auxiliar"."Segment" seo ON seo.id = bur."Segment_id"
+  INNER JOIN (
+	SELECT 
+	  bp."Booking_id", 
+	  SUM(bp."Rent") AS "Total_rent", 
+	  SUM(bp."Rent" / (1 + COALESCE(t."Value", 0) / 100) * bu."Management_fee" / 100) AS "Management_fee", 
+	  SUM(bp."Services") AS "Total_services"
+	FROM "Booking"."Booking_price" bp
+	  INNER JOIN "Booking"."Booking" b ON b.id = bp."Booking_id" 
+	  INNER JOIN "Building"."Building" bu ON bu.id = b."Building_id"
+	  INNER JOIN "Building"."Building_type" bt ON bt.id = bu."Building_type_id" 
+	  LEFT JOIN "Billing"."Tax" t ON t.id = bt."Tax_id" 
+	WHERE "Status" NOT IN ('solicitud','alternativas','alternativaspagada','descartada','descartadapagada','cancelada')
+GROUP BY 1) sums ON sums."Booking_id" = b.id
+WHERE "Status" NOT IN ('solicitud','alternativas','alternativaspagada','descartada','descartadapagada','cancelada')
+ORDER BY b.id
+;
