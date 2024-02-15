cbaa2275c6adda5e492c9d21770e89b03e90868b
alejandro.andre <alejandro.andre@experis.es>
2024-02-15 10:34:37 +0100

    Fix

--- function/Booking.Booking_group_B1_validate.sql
+++ function/Booking.Booking_group_B1_validate.sql
@@ -12,6 +12,12 @@ BEGIN
   IF NEW."Date_from" > NEW."Date_to" THEN
     RAISE exception '!!!Wrong dates!!!Fechas incorrectas!!!';
   END IF;
+  IF NEW."Date_from" > (SELECT MIN("Rent_date") FROM "Booking"."Booking_group_price" bp WHERE bp."Booking_id" = NEW.id) THEN
+    RAISE exception '!!!Rents before initial date!!!Hay rentas anteriores a la fecha de inicio!!!';
+  END IF;
+  IF NEW."Date_to" < (SELECT MAX("Rent_date") FROM "Booking"."Booking_group_price" bp WHERE bp."Booking_id" = NEW.id) THEN
+    RAISE exception '!!!Rents after finish date!!!Hay rentas posteriores a la fecha de fin!!!';
+  END IF;
 
   -- Check the places
   IF (NEW."Rooms" IS NULL OR NEW."Rooms" < 1) THEN
