28e2f6d7d255d4f253cdad4613452fb50dd81f7b
alejandro.andre <alejandro.andre@experis.es>
2024-03-30 18:38:38 +0100

    Deposit return report v0

---
+++ app/templates/report/devoluciones.devoluciones.sql
@@ -0,0 +1,48 @@
+WITH 
+"Invoices_due" AS (
+	SELECT i."Booking_id", SUM(i."Total") AS "Amount"
+	FROM "Billing"."Invoice" i 
+    INNER JOIN "Billing"."Payment" p ON p.id = i."Payment_id" 
+	WHERE p."Payment_method_id" <> 43 
+	AND p."Payment_date" IS NULL 
+	AND i."Booking_id" IS NOT NULL
+	GROUP BY 1
+	HAVING SUM(i."Total") > 0
+),
+"Invoices_from_deposit" AS (
+	SELECT i."Booking_id", SUM(i."Total") AS "Amount"
+	FROM "Billing"."Invoice" i 
+    INNER JOIN "Billing"."Payment" p ON p.id = i."Payment_id" 
+	WHERE p."Payment_method_id" = 43 
+	AND i."Booking_id" IS NOT NULL
+	GROUP BY 1
+	HAVING SUM(i."Total") > 0
+)
+SELECT 
+  b.id,
+  COALESCE(b."Check_out", b."Date_to") AS "Check_out",
+  b."Deposit_actual",
+  COALESCE(d."Amount", 0) AS "Amount_due",
+  COALESCE(g."Amount", 0) AS "Amount_from_deposit",
+  b."Damages",
+  CASE 
+	WHEN b."Status" = 'checkout' THEN 'Check-out'
+	WHEN b."Status" = 'revision' THEN 'Revisión'
+	ELSE 'Devolución fianza'
+  END AS "Status",
+  CASE 
+	WHEN b."Destination_id" IS NOT NULL THEN 'CHA' 	
+  END AS "Substatus",
+  c."Name",
+  c."Email",
+  c."Phones",
+  r."Code",
+  c."Bank_account", c."Swift", c."Bank_holder", c."Bank_name", c."Bank_address", c."Bank_city", co."Name" AS "Bank_country"
+FROM "Booking"."Booking" b
+  INNER JOIN "Customer"."Customer" c ON c.id = b."Customer_id"
+  INNER JOIN "Resource"."Resource" r ON r.id = b."Resource_id" 
+  LEFT JOIN "Geo"."Country" co ON co.id = c."Bank_country_id" 
+  LEFT JOIN "Invoices_due" d ON d."Booking_id" = b.id
+  LEFT JOIN "Invoices_from_deposit" g ON g."Booking_id" = b.id
+WHERE b."Status" IN ('checkout', 'revision', 'devolvergarantia')
+ORDER BY 2, 1 ASC
