19454956fa59b7b034032fd201bd33776740be97
aandre <alejandro.andre@experis.es>
2024-04-28 20:58:21 +0200

    Fix

--- etl/library/business/forecast.py
+++ etl/library/business/forecast.py
@@ -17,7 +17,7 @@ def forecast():
 
   workbook = openpyxl.load_workbook('csv/income_forecast.xlsx')
 
-  result = '''"id","doc_id","doc_type","booking","date","provider","customer","resource","product","amount","rate","income_type","data_type"\n''' 
+  result = '''"id","doc_id","doc_type","booking","date","provider","customer","resource","product","amount","rate","income_type","data_type","stay_length"\n''' 
   for name in ('Forecast', 'Stabilised'):
     sheet = workbook[name]
     for row in sheet.iter_rows(values_only=True):
@@ -26,7 +26,7 @@ def forecast():
             line = [
                 'C' + name[0] + row[0] + '2024' + str(month).zfill(2),
                 '-', '-', '', '2024-' + str(month).zfill(2) + '-01',
-                '', '', row[0], 'Renta mensual', round(cel, 2), round(cel, 2), 'B2X', name
+                '', '', row[0], 'Renta mensual', round(cel, 2), round(cel, 2), 'B2X', name, ""
             ]
             result += ','.join([f'"{e}"' for e in line]) + '\n'
             month += 1
