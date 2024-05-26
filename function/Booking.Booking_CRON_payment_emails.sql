17ae4735f34c63247ff57f7db78f0169804479a7
aandre <alejandro.andre@experis.es>
2024-05-26 22:21:18 +0200

    Payment automatic messages

--- function/Booking.Booking_CRON_payment_emails.sql
+++ function/Booking.Booking_CRON_payment_emails.sql
@@ -4,23 +4,23 @@ DECLARE
   entity_id INTEGER;
   customer_id INTEGER;
  
-  -- Pago no realizado hace más de 8 días (5 días + 3)
+  -- Pago no realizado hace más de 6 días
   c_first CURSOR FOR
     SELECT b.id, b."Customer_id"
     FROM "Billing"."Payment" b
     WHERE b."Payment_type" = 'servicios'
     AND b."Payment_date" IS NULL
     AND b."Payment_method_id" = 1
-    AND b."Issued_date" <= (CURRENT_DATE - INTERVAL '8 days');
+    AND b."Issued_date" <= (CURRENT_DATE - INTERVAL '6 days');
 
-  -- Pago no realizado hace más de 13 días (10 días + 3)
+  -- Pago no realizado hace más de 11 días
   c_next CURSOR FOR
     SELECT b.id, b."Customer_id"
     FROM "Billing"."Payment" b
     WHERE b."Payment_type" = 'servicios'
     AND b."Payment_date" IS NULL
     AND b."Payment_method_id" = 1
-    AND b."Issued_date" <= (CURRENT_DATE - INTERVAL '13 days');
+    AND b."Issued_date" <= (CURRENT_DATE - INTERVAL '11 days');
 
 BEGIN
 
@@ -31,7 +31,7 @@ BEGIN
   FETCH c_first INTO entity_id, customer_id;
   WHILE (FOUND) LOOP
 
-    -- Si no hay un recordatorio envia uno
+    -- Si no hay un primer recordatorio envia uno
     IF NOT EXISTS (
 		  SELECT id
   		FROM "Customer"."Customer_email"
@@ -46,19 +46,18 @@ BEGIN
   END LOOP;
   CLOSE c_first;
 
-  -- Segundd aviso
+  -- Segundo aviso
   OPEN c_next;
   FETCH c_next INTO entity_id, customer_id;
   WHILE (FOUND) LOOP
 
-    -- Si no hay un recordatorio hace menos de tres dias envia uno
+    -- Si no hay un segundo recordatorio envia uno
     IF NOT EXISTS (
 		  SELECT id
   		FROM "Customer"."Customer_email"
   		WHERE "Template" = 'pagorecallagain'
   		AND "Customer_id" = customer_id
   		AND "Entity_id" = entity_id
-  		AND "Created_at" > (CURRENT_DATE - INTERVAL '3 days')
     ) THEN
       INSERT INTO "Customer"."Customer_email" ("Customer_id", "Template", "Entity_id") VALUES (customer_id, 'pagorecallagain', entity_id);
    	END IF;
