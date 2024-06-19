a79ff69ef90fbaf3adec34b6fee994c300721d4b
aandre <alejandro.andre@experis.es>
2024-06-19 16:12:47 +0200

    Sepa

---
+++ app/templates/report/sepa.cotown.sql
@@ -0,0 +1,19 @@
+-- NetCash
+SELECT 
+  TRIM(c."Name") AS "Name", 
+  REPLACE(COALESCE(c."IBAN", ''), ' ', '') AS "IBAN",
+  LPAD(p."Booking_id"::text, 6, '0') AS "Ref_mandate",
+  TO_CHAR(p."Issued_date", 'DD/MM/YYYY') AS "Date",
+  'RCUR' AS "Sequence", 
+  LPAD(p.id::text, 6, '0') AS "Ref_order",
+  p."Amount",
+  (ARRAY['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'])[EXTRACT(MONTH FROM p."Issued_date")] AS "Concept"
+FROM "Billing"."Payment" p
+  LEFT JOIN "Customer"."Customer" c ON c.id = p."Customer_id"
+  LEFT JOIN "Billing"."Invoice" i ON i."Payment_id" = p.id
+WHERE p."Payment_method_id" = 2
+  AND p."Payment_date" IS NULL 
+  AND p."Booking_id" IS NOT NULL
+  AND p."Pos" = 'cotown'
+ORDER BY 1
+;
