d2ececc4c5f84eba7d582e7bde5248ad01dc4990
aandre <alejandro.andre@experis.es>
2024-07-25 17:43:02 +0200

    SAP loading v1

--- etl/library/business/gl.py
+++ etl/library/business/gl.py
@@ -2,10 +2,10 @@
 # Imports
 # ###################################################
 
+import re
+import sys
 import requests
 from requests.auth import HTTPBasicAuth
-
-import re
 from datetime import datetime, timezone
 
 import pandas as pd
@@ -43,9 +43,9 @@ def get_date(value):
 def gl(date, bks, company, file):
 
     params = {
-        '$select': "CFIX_ASSET_UUID,TFIX_ASSET_UUID,CACC_DOC_UUID,CPROFITCTR_UUID,TPROFITCTR_UUID,CCOST_CTR_UUID,TCOST_CTR_UUID,CCREATION_DATE,CGLACCT,TGLACCT,CFISCYEAR,CCOMPANY_UUID,TCOMPANY_UUID,CPOSTING_DATE,CDOC_DATE,COFF_BUSPARTNER,COEDREF_F_ID,COEOREF_F_ID,CFISCPER,CACC_DOC_IT_UUID,CPRODUCT_UUID,TPRODUCT_UUID,COEDPARTNER,CBUS_PART_UUID,TBUS_PART_UUID,CNOTE_HD,CNOTE_IT,CACCDOCTYPE,KCDEBIT_CURRCOMP,KCCREDIT_CURRCOMP",
+        '$select': "CCREATION_DATE,CGLACCT,TGLACCT,CPOSTING_DATE,CACC_DOC_UUID,CACC_DOC_IT_UUID,TACCDOCTYPE,CPROFITCTR_UUID,TPROFITCTR_UUID,CCOST_CTR_UUID,TCOST_CTR_UUID,CDOC_DATE,TPRODUCT_UUID,TPRODUCT_TYPE,COEDPARTNER,CBUS_PART_UUID,TBUS_PART_UUID,CNOTE_HD,CNOTE_IT,KCDEBIT_CURRCOMP,KCCREDIT_CURRCOMP",
         '$filter': "(PARA_SETOFBKS eq '" + bks + "' and PARA_COMPANY eq '" + company + "' and CCREATION_DATE ge datetime'" + date + "T00:00:00')",
-        '$orderby': "CACC_DOC_UUID",
+        '$orderby': "CACC_DOC_UUID,CACC_DOC_IT_UUID",
         '$format': "json",
         '$top': 999999
     }
@@ -55,6 +55,7 @@ def gl(date, bks, company, file):
     response = requests.get(settings.SAPURL_GL, params=params, auth=HTTPBasicAuth(settings.SAPUSER, settings.SAPPASS))
     if response.status_code != 200:
         logger.error(response.status_code)
+        logger.error(response.text)
         return
 
     # Get data
@@ -72,14 +73,23 @@ def gl(date, bks, company, file):
     # Drop unused columns
     df = df.drop(['__metadata'], axis=1)
 
+    # Remove "null"
+    df.replace('null', None, inplace=True)
+
     # Convert dates
     df['CCREATION_DATE'] = df['CCREATION_DATE'].apply(lambda x: get_date(x))
     df['CDOC_DATE'] = df['CDOC_DATE'].apply(lambda x: get_date(x))
-    df['CDOC_DATE'] = df['CDOC_DATE'].apply(lambda x: get_date(x))
     df['CPOSTING_DATE'] = df['CPOSTING_DATE'].apply(lambda x: get_date(x))
 
+    # Convert numbers
+    df[['KCCREDIT_CURRCOMP', 'KCDEBIT_CURRCOMP']] = df[['KCCREDIT_CURRCOMP', 'KCDEBIT_CURRCOMP']].fillna(0)
+    df['KCCREDIT_CURRCOMP'] = df['KCCREDIT_CURRCOMP'].astype(float)
+    df['KCDEBIT_CURRCOMP'] = df['KCDEBIT_CURRCOMP'].astype(float)
+    print(df)
+    print(df.info())
+
     # Save CSV
-    df.to_csv('csv/' + file + '.csv', index=False, quoting=csv.QUOTE_ALL)
+    df.to_csv('csv/' + file + '.csv', index=False, quoting=csv.QUOTE_MINIMAL)
 
 
 # ###################################################
