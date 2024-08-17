eb1ce446b99a97caeb885258c58f9682fedfe98a
aandre <alejandro.andre@experis.es>
2024-08-17 16:41:00 +0200

    Devoluciones changes

--- app/templates/report/devoluciones.devoluciones.sql
+++ app/templates/report/devoluciones.devoluciones.sql
@@ -66,11 +66,17 @@ SELECT
   c."Email",
   c."Phones",
   r."Code",
-  c."Bank_account", c."Swift", c."Bank_holder", c."Bank_name", c."Bank_address", c."Bank_city", co."Name" AS "Bank_country"
+  c."Bank_account", c."Swift", c."Bank_holder", c."Bank_name", c."Bank_address", c."Bank_city", co."Name" AS "Bank_country",
+  CASE 
+  WHEN co."Sepa" THEN 'SEPA'
+  ELSE ''
+  END AS "Sepa",
+  p."Name" AS "Owner"
 FROM "Booking"."Booking" b
   INNER JOIN "Customer"."Customer" c ON c.id = b."Customer_id"
   INNER JOIN "Resource"."Resource" r ON r.id = b."Resource_id" 
-  LEFT JOIN "Geo"."Country" co ON co.id = c."Bank_country_id" 
+  INNER JOIN "Provider"."Provider" p ON p.id = r."Owner_id" 
+  LEFT JOIN "Geo"."Country" co ON co."Code" = substring(c."Bank_account", 1, 2) 
   LEFT JOIN "Invoices_due" d ON d."Booking_id" = b.id
   LEFT JOIN "Invoices_from_deposit" g ON g."Booking_id" = b.id
   LEFT JOIN "Invoices_due_cotown" dc ON dc."Booking_id" = b.id
