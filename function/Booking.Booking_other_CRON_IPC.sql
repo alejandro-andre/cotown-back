11dcc85671c236c55bbfdf733c0970ad84a481d6
aandre <alejandro.andre@experis.es>
2024-08-29 16:44:24 +0200

    Fix IPC cron

--- function/Booking.Booking_other_CRON_IPC.sql
+++ function/Booking.Booking_other_CRON_IPC.sql
@@ -2,9 +2,9 @@
 BEGIN
   UPDATE "Booking"."Booking_other"
   SET 
-    "Rent" = "Rent" * (SELECT "Value_IPC"/ 100 FROM "Auxiliar"."Ipc" ORDER BY "Date_IPC" DESC LIMIT 1),
+    "Rent" = "Rent" * (SELECT 1 + "Value_IPC"/ 100 FROM "Auxiliar"."Ipc" ORDER BY "Date_IPC" DESC LIMIT 1),
     "IPC_updated" = CURRENT_DATE
   WHERE 
     EXTRACT(MONTH FROM "Date_from") = "IPC_month"
-    AND AGE(CURRENT_DATE, "IPC_updated") >= INTERVAL '1 year';
+    AND ("IPC_updated" IS NULL OR AGE(CURRENT_DATE, "IPC_updated") >= INTERVAL '1 year');
 END
