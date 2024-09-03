136be0bbd28bc487eb5e3c17ffba4e7885b1ef11
aandre <alejandro.andre@experis.es>
2024-09-03 16:30:06 +0200

    Dev

--- function/Customer.Customer_D2_delete_keycloak_user.py
+++ function/Customer.Customer_D2_delete_keycloak_user.py
@@ -1,6 +1,6 @@
 # Borra el usuario de cliente en Keycloak
 # AFTER DELETE
 from urllib.request import urlopen
-response = urlopen('https://dev.cotown.ciber.es/api/v1/customeruser/del/' + str(TD['old']['id']))
+response = urlopen('https://back.cotown.oimbra.tech/api/v1/customeruser/del/' + str(TD['old']['id']))
 response.close()
 return None
