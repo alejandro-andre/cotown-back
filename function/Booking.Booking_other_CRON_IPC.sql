09d8292d9099611dfc3e36b38c56c65ee7f00800
aandre <alejandro.andre@experis.es>
2024-10-13 07:12:45 +0200

    Fix IPC updating

--- function/Booking.Booking_other_CRON_IPC.sql
+++ function/Booking.Booking_other_CRON_IPC.sql
@@ -6,5 +6,6 @@ BEGIN
     "IPC_updated" = CURRENT_DATE
   WHERE 
     EXTRACT(MONTH FROM "Date_from") = "IPC_month"
-    AND ("IPC_updated" IS NULL OR AGE(CURRENT_DATE, "IPC_updated") >= INTERVAL '1 year');
+  AND 
+    EXTRACT(YEAR FROM "IPC_updated") < EXTRACT(YEAR FROM CURRENT_DATE);
 END
