34f15e747a665e76e7193ff3d77ac409c1c7ca6b
aandre <alejandro.andre@experis.es>
2024-12-02 18:38:00 +0100

    LAU % contribution

---
+++ function/Booking.Booking_other_B1_validate.sql
@@ -0,0 +1,22 @@
+-- Verifica los datos de la reserva
+DECLARE
+
+  pre_capex_diff DECIMAL;
+
+BEGIN
+
+  -- Get pre-capex values
+  SELECT COALESCE(r."Pre_capex_vacant", 0) - COALESCE(r."Pre_capex_long_term", 0)
+  INTO pre_capex_diff
+  FROM "Resource"."Resource" r
+  WHERE r.id = NEW."Resource_id";
+
+  -- Calc recommended contribution
+  IF NEW."Contribution_recommended" IS NULL AND NEW."Contribution_percent" IS NOT NULL THEN
+    NEW."Contribution_recommended" := COALESCE(NEW."Contribution_percent", 0) * pre_capex_diff / 100;
+  END IF;
+
+  -- Return record
+  RETURN NEW;
+
+END;
