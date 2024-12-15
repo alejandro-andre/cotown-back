7a47853fad40351e55b0200dcdd15e8d9809db1d
aandre <alejandro.andre@experis.es>
2024-12-15 22:01:20 +0100

    Docusign v0.1

--- app/library/api/misc.py
+++ app/library/api/misc.py
@@ -43,7 +43,26 @@ def req_pub_hello():
 # Post
 def req_pub_post():
 
-  logger.debug(json.dumps(request.json, indent=4))
+  # Get data
+  data = request.json.get('data')
+  if not data:
+    return 'KO'
+
+  # Get event info
+  event  = request.json['event']
+  status = data['envelopeSummary']['status']
+  id     = data['envelopeId']
+  dt     = data['envelopeSummary']['statusChangedDateTime']
+  if status not in ('sent', 'delivered', 'declined', 'completed', 'expired'):
+     status = 'other'
+
+  # Debug
+  logger.info(id)
+  logger.info(dt)
+  logger.info(event)
+  logger.info(status)
+
+  # Return
   return 'OK'
 
 
