e0659c3b81ddedc703500bbebe61e77ba1b09071
aandre <alejandro.andre@experis.es>
2025-01-07 15:58:27 +0100

    SEPA fix

--- app/templates/report/sepa.vandor.sql
+++ app/templates/report/sepa.vandor.sql
@@ -14,10 +14,14 @@ SELECT
   	WHEN 'deposito' THEN 'DEPOSITO'
   	WHEN 'checkin' THEN 'CHECKIN'
   	ELSE 'RENTA ' || (ARRAY['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'])[EXTRACT(MONTH FROM p."Issued_date")] 
-  END AS "Concept"
+  END AS "Concept",
+  (a."Name" || ' - ' || at."Name") AS "Agent"
 FROM "Billing"."Payment" p
   LEFT JOIN "Customer"."Customer" c ON c.id = p."Customer_id"
-  LEFT JOIN "Geo"."Country" co on co.id = c."Country_id"
+  LEFT JOIN "Geo"."Country" co ON co.id = c."Country_id"
+  LEFT JOIN "Booking"."Booking" b ON b.id = p."Booking_id"
+  LEFT JOIN "Provider"."Agent" a ON a.id = b."Agent_id"
+  LEFT JOIN "Provider"."Agent_type" at ON at.id = a."Agent_type_id"
 WHERE p."Payment_method_id" = 2
   AND p."Payment_date" IS NULL 
   AND p."Booking_id" IS NOT NULL
