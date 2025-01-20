35beade03201a4c33eb7d390f8aea24c77a37da7
aandre <alejandro.andre@experis.es>
2025-01-20 18:32:17 +0100

    Fix

--- app/batch_billing.py
+++ app/batch_billing.py
@@ -1117,6 +1117,7 @@ def bill_lau(dbClient, con):
         AND i."Issued_date" >= DATE_TRUNC('month', CURRENT_DATE + INTERVAL '3 days') 
         AND i."Issued_date" < (DATE_TRUNC('month', CURRENT_DATE + INTERVAL '3 days') + INTERVAL '1 month')
     WHERE i.id IS NULL
+      AND bo."Rent" IS NOT NULL
       AND bo."Unlawful" <> TRUE
     ;
     ''')
@@ -1234,6 +1235,7 @@ def bill_lau(dbClient, con):
       # Commit
       con.commit()
       num += 1
+      break
 
     # Process exception
     except Exception as error:
@@ -1314,7 +1316,7 @@ def main():
   pay_bills(dbClient, con)
 
   # 5. Bill LAU/Others
-  #?bill_lau(dbClient, con)
+  bill_lau(dbClient, con)
 
   # Disconnect
   dbClient.putconn(con)
