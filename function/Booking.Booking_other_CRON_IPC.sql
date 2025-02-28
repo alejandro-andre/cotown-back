f56590a94df08caad68e879bed83393e4e121834
aandre <alejandro.andre@experis.es>
2025-02-28 13:10:14 +0100

    IPC

--- function/Booking.Booking_other_CRON_IPC.sql
+++ function/Booking.Booking_other_CRON_IPC.sql
@@ -1,9 +1,16 @@
 -- Update IPC
 BEGIN
-  --UPDATE "Booking"."Booking_other"
-  --SET 
-  --  "Rent" = "Rent" * (SELECT 1 + "Value_IPC"/ 100 FROM "Auxiliar"."Ipc" ORDER BY "Date_IPC" DESC LIMIT 1),
-  --  "IPC_updated" = CURRENT_DATE
-  --WHERE EXTRACT(MONTH FROM CURRENT_DATE + INTERVAL '1 month') = "IPC_month"
-  --  AND EXTRACT(YEAR FROM CURRENT_DATE + INTERVAL '1 month') > EXTRACT(YEAR FROM "IPC_updated");
+  UPDATE "Booking"."Booking_other"
+  SET
+    "Prev_rent" = "Rent",
+  WHERE ("Date_to" > CURRENT_DATE OR "Date_to" IS NULL)
+    AND EXTRACT(MONTH FROM CURRENT_DATE + INTERVAL '1 month') = "IPC_month"
+    AND EXTRACT(YEAR FROM CURRENT_DATE + INTERVAL '1 month') > EXTRACT(YEAR FROM "IPC_updated");
+  UPDATE "Booking"."Booking_other"
+  SET
+    "Rent" = ROUND(100 * "Rent" * (SELECT 1 + "Value_IPC"/ 100 FROM "Auxiliar"."Ipc" ORDER BY "Date_IPC" DESC LIMIT 1)) / 100,
+    "IPC_updated" = CURRENT_DATE
+  WHERE ("Date_to" > CURRENT_DATE OR "Date_to" IS NULL)
+    AND EXTRACT(MONTH FROM CURRENT_DATE + INTERVAL '1 month') = "IPC_month"
+    AND EXTRACT(YEAR FROM CURRENT_DATE + INTERVAL '1 month') > EXTRACT(YEAR FROM "IPC_updated");
 END
