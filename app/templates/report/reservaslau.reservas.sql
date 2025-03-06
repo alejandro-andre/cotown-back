561741c1c51d524552d04ec84a89a614988e4072
aandre <alejandro.andre@experis.es>
2025-03-06 17:18:22 +0100

    Improvements

--- app/templates/report/reservaslau.reservas.sql
+++ app/templates/report/reservaslau.reservas.sql
@@ -1,6 +1,6 @@
 SELECT 
   bo.id, bo."Date_from", bo."Date_to", bo."Date_estimated", 
-  p."Name" as "Product_name", bo."Unlawful", bo."Rent", m."Name" AS "Month", bo."IPC_updated",
+  p."Name" as "Product_name", bo."Rent", m."Name" AS "Month", bo."IPC_updated",
   bo."Deposit", bo."Deposit_required", bo."Deposit_returned", bo."Deposit_return_date",
   bo."Compensation", bo."Compensation_date",
   bo."Extras", bo."Extras_concept", bo."Debts", bo."Warrants", 
@@ -10,7 +10,16 @@ SELECT
   bo."Comments", 
   bu."Name" AS "Building_name", r."Code",
   c."Name" AS "Customer_name", c."Document", c."Phones",
-  bs."Name" AS "Status_name"
+  bs."Name" AS "Status_name",
+  CASE
+    WHEN bo."Unlawful" THEN 'Yes'
+    ELSE ''
+  END AS "Unlawful", 
+  CASE
+    WHEN bo."Unlawful" AND bo."Bill_unlawful" THEN 'Yes'
+    WHEN bo."Unlawful" AND NOT bo."Bill_unlawful" THEN 'No'
+    ELSE ''
+  END AS "Bill_unlawful"
 FROM "Booking"."Booking_other" bo 
   INNER JOIN "Resource"."Resource" r ON r.id = bo."Resource_id"
   INNER JOIN "Building"."Building" bu ON bu.id = r."Building_id" 
