57c17806c094dc095f65938909cc4ac44b120679
aandre <alejandro.andre@experis.es>
2025-03-06 14:55:26 +0100

    LTC new

--- app/templates/report/ltc.ltc.sql
+++ app/templates/report/ltc.ltc.sql
@@ -5,7 +5,8 @@ SELECT
   r."Pre_capex_long_term", r."Pre_capex_vacant", r."Post_capex", r."Capex",
   g."Name_en" AS "Gender", 
   c."Birth_date",
-  etl.labels[array_position(et.values, bo."Pending_subrogation"::text)]::text AS "Pending_subrogation",
+  etsl.labels[array_position(ets.values, bo."Pending_subrogation"::text)]::text AS "Pending_subrogation",
+  etnl.labels[array_position(etn.values, bo."Negotiation"::text)]::text AS "Negotiation",
   bo."Date_to",
   bo."Date_estimated",
   CASE
@@ -17,8 +18,7 @@ SELECT
   bo."Contribution_asking", 
   bo."Contribution_proposal", 
   bo."Contribution_proposed_date", 
-  bo."Contribution_comments",
-  bo."Comments",
+  bs."Name_en" AS "Substatus_name",
   CASE
     WHEN bo."Date_estimated" IS NULL AND bo."Date_to" IS NULL THEN 'LTNC'
     WHEN bo."Date_estimated" IS NOT NULL AND bo."Date_to" IS NULL THEN 'LTC'
@@ -26,6 +26,7 @@ SELECT
   END AS "Tenancy_type"
 FROM "Resource"."Resource" r
   INNER JOIN "Booking"."Booking_other" bo ON bo."Resource_id" = r.id
+  INNER JOIN "Booking"."Booking_subtype" bs ON bs.id = bo."Substatus_id" 
   INNER JOIN "Billing"."Product" p ON p.id = bo."Product_id" 
   LEFT JOIN "Resource"."Resource_status" rs ON rs.id = r."Status_id"
   LEFT JOIN "Building"."Building" bu ON bu.id = r."Building_id" 
@@ -33,8 +34,12 @@ FROM "Resource"."Resource" r
   LEFT JOIN "Geo"."Location" l ON l.id = d."Location_id" 
   LEFT JOIN "Customer"."Customer" c ON c.id = bo."Customer_id" 
   LEFT JOIN "Auxiliar"."Gender" g ON g.id = c."Gender_id"
-  INNER JOIN "Models"."EnumType" et ON et.id = 24
-  INNER JOIN "Models"."EnumTypeLabel" etl ON etl.container = et.id AND etl.locale = 'en_US'
+  INNER JOIN "Models"."EnumType" ets ON ets.id = 24
+  INNER JOIN "Models"."EnumType" etn ON etn.id = 28
+  INNER JOIN "Models"."EnumTypeLabel" etsl ON etsl.container = ets.id AND etsl.locale = 'en_US'
+  INNER JOIN "Models"."EnumTypeLabel" etnl ON etnl.container = etn.id AND etnl.locale = 'en_US'
 WHERE p."Name" LIKE 'Renta (LAU)%'
-AND r."Status_id" IN (2, 3)
+  AND r."Status_id" IN (2, 3)
+  AND bo."Substatus_id" <> 6
 ORDER BY "Tenancy_type" DESC, 1
+;
