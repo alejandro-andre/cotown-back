ccac106bf7586e0edb52ff38db3183848e791d02
aandre <alejandro.andre@experis.es>
2025-03-06 14:55:22 +0100

    Fix

--- function/Booking.Booking_other_CRON_IPC.sql
+++ function/Booking.Booking_other_CRON_IPC.sql
@@ -2,7 +2,7 @@
 BEGIN
   UPDATE "Booking"."Booking_other"
   SET
-    "Prev_rent" = "Rent",
+    "Prev_rent" = "Rent"
   WHERE ("Date_to" > CURRENT_DATE OR "Date_to" IS NULL)
     AND EXTRACT(MONTH FROM CURRENT_DATE + INTERVAL '1 month') = "IPC_month"
     AND EXTRACT(YEAR FROM CURRENT_DATE + INTERVAL '1 month') > EXTRACT(YEAR FROM "IPC_updated");
