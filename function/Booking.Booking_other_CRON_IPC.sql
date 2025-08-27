c4971f7d6257dd98e227581ebb1a55cd91150835
alejandro-andre <alejandroandref@gmail.com>
2025-08-27 12:23:57 +0200

    IPC Cron

--- function/Booking.Booking_other_CRON_IPC.sql
+++ function/Booking.Booking_other_CRON_IPC.sql
@@ -5,34 +5,44 @@ DECLARE
   rec RECORD;
 
   -- IPC en dos meses
-  ipc_cur CURSOR FOR
+  ipc_notify CURSOR FOR
   SELECT *
-	FROM "Booking"."Booking_other"
-	WHERE ("Date_to" > CURRENT_DATE OR "Date_to" IS NULL)
-	  AND EXTRACT(MONTH FROM CURRENT_DATE + INTERVAL '2 month') = "IPC_month"
-	  AND EXTRACT(YEAR FROM "IPC_updated") < EXTRACT(YEAR FROM CURRENT_DATE - INTERVAL '1 month');
+  FROM "Booking"."Booking_other"
+  WHERE ("Date_to" > CURRENT_DATE OR "Date_to" IS NULL)
+    AND EXTRACT(MONTH FROM CURRENT_DATE + INTERVAL '2 month') = "IPC_month"
+    AND ("IPC_notified" IS NULL OR EXTRACT(YEAR FROM "IPC_notified") < EXTRACT(YEAR FROM CURRENT_DATE + INTERVAL '2 month'));
+
+  -- IPC proximo mes
+  ipc_update CURSOR FOR
+  SELECT *
+  FROM "Booking"."Booking_other"
+  WHERE ("Date_to" > CURRENT_DATE OR "Date_to" IS NULL)
+    AND EXTRACT(MONTH FROM CURRENT_DATE + INTERVAL '1 month') = "IPC_month"
+    AND ("IPC_updated" IS NULL OR EXTRACT(YEAR FROM "IPC_updated") < EXTRACT(YEAR FROM CURRENT_DATE + INTERVAL '1 month'));
 
 BEGIN
 
   -- Obtener el último valor del IPC
-  SELECT "Value_IPC" INTO ipc_value FROM "Auxiliar"."Ipc" ORDER BY "Date_IPC" DESC LIMIT 1;
+  BEGIN
+    SELECT "Value_IPC" INTO ipc_value FROM "Auxiliar"."Ipc" ORDER BY "Date_IPC" DESC LIMIT 1;
+  EXCEPTION WHEN NO_DATA_FOUND THEN
+    RAISE EXCEPTION 'No hay valores de IPC en Auxiliar.Ipc';
+  END;
 
   -- Abrir cursor y recorrer fila a fila
-  OPEN ipc_cur;
-  FETCH ipc_cur INTO rec;
-  WHILE (FOUND) LOOP
+  OPEN ipc_notify;
+  FETCH ipc_notify INTO rec;
+  WHILE FOUND LOOP
 
     -- Actualizar fila
     UPDATE "Booking"."Booking_other"
     SET
-      "Prev_rent" = rec."Rent",
-      "Rent" = ROUND(rec."Rent" * (1 + ipc_value / 100) * 100) / 100,
       "Applied_IPC" = ipc_value,
-      "IPC_updated" = CURRENT_DATE
+      "IPC_notified" = CURRENT_DATE
     WHERE id = rec.id;
 
     -- Mostrar valores
-    RAISE NOTICE 'Booking: %, Renta anterior: %, IPC aplicado: %, Renta nueva: %', rec.id, rec."Rent", ipc_value, ROUND(rec."Rent" * (1 + ipc_value / 100) * 100) / 100;
+    RAISE NOTICE 'Booking: %, IPC aplicado: %', rec.id, ipc_value;
 
     -- Enviar correo
     IF rec."Send_IPC" THEN
@@ -40,8 +50,34 @@ BEGIN
     END IF;
 
     -- Siguiente
-    FETCH ipc_cur INTO rec;
+    FETCH ipc_notify INTO rec;
   END LOOP;
-  CLOSE ipc_cur;
+  CLOSE ipc_notify;
+
+  -- Abrir cursor y recorrer fila a fila
+  OPEN ipc_update;
+  FETCH ipc_update INTO rec;
+  WHILE FOUND LOOP
+
+    -- Actualizar fila
+    UPDATE "Booking"."Booking_other"
+    SET
+      "Prev_rent" = rec."Rent",
+      "Rent" = ROUND(rec."Rent" * (1 + COALESCE(rec."Applied_IPC", 0) / 100), 2),
+      "IPC_updated" = CURRENT_DATE
+    WHERE id = rec.id;
+
+    -- Mostrar valores
+    RAISE NOTICE 
+      'Booking: %, Renta anterior: %, IPC aplicado: %, Renta nueva: %', 
+      rec.id, 
+      rec."Rent", 
+      COALESCE(rec."Applied_IPC", 0), 
+      ROUND(rec."Rent" * (1 + COALESCE(rec."Applied_IPC", 0) / 100), 2);
+
+    -- Siguiente
+    FETCH ipc_update INTO rec;
+  END LOOP;
+  CLOSE ipc_update;
 
 END;
