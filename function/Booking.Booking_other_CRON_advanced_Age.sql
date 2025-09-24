af27efe9b200d047739cd8f74033305ab715a848
alejandro-andre <alejandroandref@gmail.com>
2025-09-24 21:57:06 +0200

    Advanced age

--- function/Booking.Booking_other_CRON_advanced_Age.sql
+++ function/Booking.Booking_other_CRON_advanced_Age.sql
@@ -1,8 +1,20 @@
--- Actualiza la fecha de fin de contrato para edad avanzada (1 año)
+-- Actualiza la fecha de fin de contrato para edad avanzada (2 años)
 BEGIN
   
+  -- Actualiza el estado de las reservas de mayores de 90 años
   UPDATE "Booking"."Booking_other" b
-  SET "Date_estimated" = date_trunc('month', current_date + interval '1 year')::date
+  SET "Substatus_id" = 2
+  FROM "Customer"."Customer" c
+  WHERE c.id = b."Customer_id"
+    AND c."Birth_date" < (CURRENT_DATE - INTERVAL '90 years')
+    AND b."Date_estimated" > CURRENT_DATE
+    AND b."Substatus_id" <> 2;
+
+  -- Actualiza fin de contrato
+  UPDATE "Booking"."Booking_other" b
+  SET "Date_estimated" = (date_trunc('month', current_date + interval '2 year') + interval '1 month' - interval '1 day')::date,
+      "Date_precapex" = NULL,
+      "Date_capex" = NULL
   WHERE b."Substatus_id" = 2;
 
 END;
