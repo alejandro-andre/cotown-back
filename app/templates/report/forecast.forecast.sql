4d5aeaf89b8f36da3993ffe7b47e8499e6e2a645
alejandro-andre <alejandroandref@gmail.com>
2025-10-08 21:58:07 +0200

    Forecast change v1

--- app/templates/report/forecast.forecast.sql
+++ app/templates/report/forecast.forecast.sql
@@ -1,74 +1,86 @@
 WITH
-"Extras" AS (
-  -- Extras, por plaza
-  SELECT r.id,
-    EXP(SUM(LN(1 + COALESCE(rat."Increment", 0) / 100))) AS "Extra"
-  FROM "Resource"."Resource" r
-    LEFT JOIN "Resource"."Resource_amenity" ra ON ra."Resource_id" = r.id 
-    LEFT JOIN "Resource"."Resource_amenity_type" rat ON rat.id = ra."Amenity_type_id" 
-  GROUP BY 1
-),
-"Details" AS (
-  WITH
-  "Dates" AS (
-    -- Meses del año y cambio de curso
-    SELECT 
-      date_trunc('month', generate_series)::date AS "Date",
-      CASE 
-    	WHEN EXTRACT(MONTH FROM generate_series) < 9 THEN EXTRACT(YEAR FROM generate_series) 
-        ELSE 1 + EXTRACT(YEAR FROM generate_series) 
-      END AS "Year"
-    FROM 
-      generate_series(%(fdesde)s::date, %(fhasta)s::date - '1 day'::interval, '1 month'::interval)
-  )
-  -- Detalles por plaza
-  SELECT
-    r."Code",
-    r."Resource_type",
-    rft."Code",
-    rpt."Code",
-    substring(r."Code", 1, 12) AS "Resource",
-    r.id,
-    CASE
-      WHEN r."Resource_type" = 'piso'THEN r.id
-      ELSE r."Flat_id"
-    END AS "Flat_id",
-    d."Date", 
-    CASE
-      WHEN EXISTS (
-        SELECT ra.id 
-        FROM "Resource"."Resource_availability" ra 
-        INNER JOIN "Resource"."Resource_status" rs on rs.id = ra."Status_id"
-        WHERE NOT rs."Available" AND ra."Resource_id" = r."Flat_id" AND ra."Date_from" <= d."Date" AND ra."Date_to" >= d."Date"
-      ) THEN 0
-      ELSE 1
-    END AS "Beds",
-    pr."Multiplier",
-    pd."Rent_short" * pr."Multiplier" AS "Rent_short",
-    pd."Rent_medium" * pr."Multiplier" AS "Rent_medium",
-    pd."Rent_long" * pr."Multiplier" AS "Rent_long",
-    pd."Rent_group" * pr."Multiplier" AS "Rent_group",
-    r."Management_fee"
-  FROM "Resource"."Resource" r
-    CROSS JOIN "Dates" d
-    INNER JOIN "Building"."Building" b ON b."id" = r."Building_id" 
-    LEFT JOIN "Resource"."Resource_flat_type" rft ON rft.id = r."Flat_type_id" 
-    LEFT JOIN "Resource"."Resource_place_type" rpt ON rpt.id = r."Place_type_id" 
-    LEFT JOIN "Billing"."Pricing_detail" pd ON pd."Building_id" = r."Building_id" 
-      AND pd."Flat_type_id" = r."Flat_type_id"
-      AND COALESCE(pd."Place_type_id", 0) = COALESCE(r."Place_type_id", 0)
-    LEFT JOIN "Billing"."Pricing_rate" pr ON pr.id = r."Rate_id"
-  WHERE (pd."Year" = d."Year" OR pd."Year" IS NULL)
-    AND (
-      r."Resource_type" = 'plaza'
-      OR (r."Resource_type" = 'habitacion' AND rpt."Code" NOT LIKE 'DUI%%')
-      OR (r."Resource_type" = 'piso' AND rft."Code" = 'APT1')
+  "Details" AS (
+    WITH 
+    "Extras" AS (
+      -- Extras, por plaza
+      SELECT r.id,
+        EXP(SUM(LN(1 + COALESCE(rat."Increment", 0) / 100))) AS "Extra"
+      FROM "Resource"."Resource" r
+        LEFT JOIN "Resource"."Resource_amenity" ra ON ra."Resource_id" = r.id 
+        LEFT JOIN "Resource"."Resource_amenity_type" rat ON rat.id = ra."Amenity_type_id" 
+      GROUP BY 1
+    ),
+    "Dates" AS (
+      -- Meses del año y cambio de curso
+      SELECT 
+        date_trunc('month', generate_series)::date AS "Date",
+        CASE 
+      	WHEN EXTRACT(MONTH FROM generate_series) < 9 THEN EXTRACT(YEAR FROM generate_series) 
+          ELSE 1 + EXTRACT(YEAR FROM generate_series) 
+        END AS "Year"
+      FROM 
+        generate_series(%(fdesde)s::date, %(fhasta)s::date - '1 day'::interval, '1 month'::interval)
     )
-  ORDER BY 1
-)
+    -- Detalles por plaza
+    SELECT
+      r."Code",
+      r."Resource_type",
+      rft."Code",
+      rpt."Code",
+      substring(r."Code", 1, 12) AS "Resource",
+      r.id,
+      e."Extra",
+      CASE
+        WHEN r."Resource_type" = 'piso'THEN r.id
+        ELSE r."Flat_id"
+      END AS "Flat_id",
+      d."Date", 
+      CASE
+        WHEN EXISTS (
+          SELECT ra.id 
+          FROM "Resource"."Resource_availability" ra 
+          INNER JOIN "Resource"."Resource_status" rs on rs.id = ra."Status_id"
+          WHERE NOT rs."Available" AND ra."Resource_id" = r."Flat_id" AND ra."Date_from" <= d."Date" AND ra."Date_to" >= d."Date"
+        ) THEN 0
+        ELSE 1
+      END AS "Beds",
+      pr."Multiplier",
+      pd."Rent_short" * pr."Multiplier" * e."Extra" AS "Rent_short",
+      pd."Rent_medium" * pr."Multiplier" * e."Extra" AS "Rent_medium",
+      pd."Rent_long" * pr."Multiplier" * e."Extra" AS "Rent_long",
+      pd."Rent_group" * pr."Multiplier" * e."Extra" AS "Rent_group",
+      r."Management_fee"
+    FROM "Resource"."Resource" r
+      CROSS JOIN "Dates" d
+      INNER JOIN "Building"."Building" b ON b."id" = r."Building_id" 
+      LEFT JOIN "Extras" e ON r.id = e.id
+      LEFT JOIN "Resource"."Resource_flat_type" rft ON rft.id = r."Flat_type_id" 
+      LEFT JOIN "Resource"."Resource_place_type" rpt ON rpt.id = r."Place_type_id" 
+      LEFT JOIN "Billing"."Pricing_detail" pd ON pd."Building_id" = r."Building_id" 
+        AND pd."Flat_type_id" = r."Flat_type_id"
+        AND COALESCE(pd."Place_type_id", 0) = COALESCE(r."Place_type_id", 0)
+      LEFT JOIN "Billing"."Pricing_rate" pr ON pr.id = r."Rate_id"
+    WHERE (pd."Year" = d."Year" OR pd."Year" IS NULL)
+      AND (
+        r."Resource_type" = 'plaza'
+        OR (r."Resource_type" = 'habitacion' AND rpt."Code" NOT LIKE 'DUI%%')
+        OR (r."Resource_type" = 'piso' AND rft."Code" = 'APT1')
+      )
+      AND NOT EXISTS (
+        SELECT 1
+        FROM "Booking"."Booking_detail" bd
+          INNER JOIN "Resource"."Resource_availability" ra ON ra.id = bd."Availability_id"
+          INNER JOIN "Resource"."Resource_status" rs ON rs.id = ra."Status_id"
+        WHERE bd."Resource_id" = r.id 
+          AND bd."Date_from" <= d."Date" 
+          AND bd."Date_to" >= d."Date"
+          AND NOT rs."Available"
+      )
+    ORDER BY 1, "Date"
+  )
 SELECT
-  p."Date" AS "Date_price",
   p."Resource" AS "Code",
+  p."Date" AS "Date_price",
   COALESCE(rf."Occupancy", 0) / 100 AS "Occupancy",
   COALESCE(rf."Rent_long", 0) AS "Rent_long",
   COALESCE(rf."Rent_medium", 0) AS "Rent_medium",
@@ -84,13 +96,12 @@ SELECT
   COALESCE(rf."Reinvoices", 0) AS "Reinvoices",
   COALESCE(rf."Beds", 0) AS "Beds",
   SUM(p."Beds") AS "Available_beds",
-  ROUND(AVG(p."Rent_short" * e."Extra"), 2) AS "Short",
-  ROUND(AVG(p."Rent_medium" * e."Extra"), 2) AS "Medium",
-  ROUND(AVG(p."Rent_long" * e."Extra"), 2) AS "Long",
+  ROUND(AVG(p."Rent_short"), 2) AS "Short",
+  ROUND(AVG(p."Rent_medium"), 2) AS "Medium",
+  ROUND(AVG(p."Rent_long"), 2) AS "Long",
   ROUND(AVG(p."Rent_group"), 2) AS "Group",
   MAX(p."Management_fee") / 100.0 AS "Management_fee"
 FROM "Details" p
-  LEFT JOIN "Extras" e ON p.id = e.id
   LEFT JOIN "Resource"."Resource_forecast" rf ON rf."Resource_id" = p."Flat_id" AND rf."Date_price" = p."Date"
 GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
-ORDER BY 2, 1;
+ORDER BY 1, 2;
