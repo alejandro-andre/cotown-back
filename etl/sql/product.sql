62c3d11e495b1b4e0b2679d7306199acffeb813a
alejandro-andre <alejandroandref@gmail.com>
2025-10-10 12:09:11 +0200

    DW products

--- etl/sql/product.sql
+++ etl/sql/product.sql
@@ -1,3 +1,5 @@
+SELECT 'Management fee' AS "id", 'Fee' AS "type"
+UNION
 SELECT p."Name_en" AS "id", pt."Name_en" AS "type"
 FROM "Billing"."Product" p
 INNER JOIN "Billing"."Product_type" pt ON pt.id = p."Product_type_id";
