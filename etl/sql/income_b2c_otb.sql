961a7f1a7c4146bd9032d5ef72f382a011b7d0c3
alejandro-andre <alejandroandref@gmail.com>
2025-12-02 13:41:01 +0100

    Income other concepts

--- etl/sql/income_b2c_otb.sql
+++ etl/sql/income_b2c_otb.sql
@@ -1,3 +1,4 @@
+-- Rent
 SELECT 
   CONCAT('ICOR', bp.id) AS "id",
   b.id AS "doc_id",
@@ -40,6 +41,7 @@ WHERE bp."Rent_date" >= '2024-01-01'
 
 UNION
 
+-- Services
 SELECT 
   CONCAT('ICOS', bp.id) AS "id",
   b.id AS "doc_id",
@@ -83,4 +85,88 @@ WHERE bp."Rent_date" >= '2024-01-01'
   AND bp."Invoice_rent_id" IS NULL AND bp."Invoice_services_id" IS NULL
   AND b."Status" IN ('confirmada', 'firmacontrato', 'checkinconfirmado', 'contrato','checkin', 'inhouse', 'checkout', 'revision')
   AND bp."Services" > 0
+
+UNION
+
+-- Other non recurring
+SELECT
+  CONCAT('ICOON', bs.id) AS "id",
+  bs."Booking_id" AS "doc_id",
+  '-' AS "doc_type",
+  'C' || bs."Booking_id"::text AS "booking",
+  bs."Billing_date_from" AS "date",
+  p."Document" AS "provider",
+  b."Customer_id" AS "customer",
+  r."Code" AS "resource",
+  CASE
+    WHEN EXTRACT(MONTH FROM AGE(b."Date_to", b."Date_from")) < 3 THEN 'SHORT'
+    WHEN EXTRACT(MONTH FROM AGE(b."Date_to", b."Date_from")) < 7 THEN 'MEDIUM'
+    ELSE 'LONG'
+  END AS "stay_length",
+  pr."Name_en" AS "product",
+  bs."Amount" / (1 + (t."Value" / 100)) AS "amount",
+  bs."Amount" / (1 + (t."Value" / 100)) AS "rate",
+  bs."Amount" / (1 + (t."Value" / 100)) AS "price",
+  CASE
+    WHEN b."Status" = 'confirmada' THEN 'Tentative' 
+    ELSE 'OTB' 
+  END AS "data_type",
+  NULL AS "discount_type"
+FROM "Booking"."Booking_service" bs 
+  INNER JOIN "Booking"."Booking" b ON b.id = bs."Booking_id" 
+  INNER JOIN "Provider"."Provider" p ON p.id = 10
+  INNER JOIN "Resource"."Resource" r ON r.id = b."Resource_id" 
+  INNER JOIN "Building"."Building" bu on bu.id = r."Building_id"
+  INNER JOIN "Billing"."Product" pr ON pr.id = bs."Product_id" 
+  INNER JOIN "Billing"."Tax" t ON t.id = bs."Tax_id"
+WHERE bs."Invoice_services_id" IS NULL 
+  AND bs."Billing_date_from" IS NOT NULL 
+  AND bs."Extra_type" <> 'recurrente'
+  AND bs."Amount" > 0
+  AND b."Status" IN ('confirmada', 'firmacontrato', 'checkinconfirmado', 'contrato','checkin', 'inhouse', 'checkout', 'revision')
+
+UNION
+
+-- Other recurring
+SELECT
+  CONCAT('ICOOR', bs.id, to_char(d.dt, 'YYYYMM')) AS "id",
+  bs."Booking_id" AS "doc_id",
+  '-' AS "doc_type",
+  'C' || bs."Booking_id"::text AS "booking",
+  d.dt AS "date", -- día 1 de cada mes
+  p."Document" AS "provider",
+  b."Customer_id" AS "customer",
+  r."Code" AS "resource",
+  CASE
+    WHEN EXTRACT(MONTH FROM AGE(b."Date_to", b."Date_from")) < 3 THEN 'SHORT'
+    WHEN EXTRACT(MONTH FROM AGE(b."Date_to", b."Date_from")) < 7 THEN 'MEDIUM'
+    ELSE 'LONG'
+  END AS "stay_length",
+  pr."Name_en" AS "product",
+  bs."Amount" / (1 + (t."Value" / 100)) AS "amount",
+  bs."Amount" / (1 + (t."Value" / 100)) AS "rate",
+  bs."Amount" / (1 + (t."Value" / 100)) AS "price",
+  CASE
+    WHEN b."Status" = 'confirmada' THEN 'Tentative'
+    ELSE 'OTB'
+  END AS "data_type",
+  NULL AS "discount_type"
+FROM "Booking"."Booking_service" bs
+  JOIN "Booking"."Booking" b ON b.id = bs."Booking_id"
+  JOIN "Provider"."Provider" p ON p.id = 10
+  JOIN "Resource"."Resource" r ON r.id = b."Resource_id"
+  JOIN "Building"."Building" bu ON bu.id = r."Building_id"
+  JOIN "Billing"."Product" pr ON pr.id = bs."Product_id"
+  JOIN "Billing"."Tax" t ON t.id = bs."Tax_id"
+  JOIN LATERAL (
+    SELECT generate_series(
+      date_trunc('month', bs."Billing_date_from"),
+      date_trunc('month', bs."Billing_date_to"),
+      interval '1 month'
+    ) AS dt
+) d ON TRUE
+WHERE bs."Invoice_services_id" IS NULL
+  AND bs."Extra_type" = 'recurrente'
+  AND bs."Amount" > 0
+  AND b."Status" IN ('confirmada', 'firmacontrato', 'checkinconfirmado', 'contrato','checkin', 'inhouse', 'checkout', 'revision')
 ;
