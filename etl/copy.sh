5c0f4bf71d25155a42e09788f6c578eb689966c4
alejandro-andre <alejandroandref@gmail.com>
2026-01-20 16:04:36 +0100

    Copy Stabilised to BP v0.3

--- etl/copy.sh
+++ etl/copy.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+DATE_DIR=$(date +%Y-%m-%d)
+BACKUP_DIR="csv/$DATE_DIR"
+
+mkdir -p "$BACKUP_DIR"
+
 sed -e 's/ISA/XSA/g' -e 's/ISC/XSC/g' -e 's/Stabilised/Business Plan/g' csv/income_stabilised.csv > csv/income_business_plan.csv
 sed -e 's/OSA/XSA/g' -e 's/OSC/XSC/g' -e 's/Stabilised/Business Plan/g' csv/occupancy_stabilised.csv > csv/occupancy_business_plan.csv
 sed -e 's/BDR/XDR/g' -e 's/Real/Business Plan/g' csv/beds_real.csv > csv/beds_business_plan.csv
 
+cp csv/income_business_plan.csv csv/occupancy_business_plan.csv csv/beds_business_plan.csv "$BACKUP_DIR/"
