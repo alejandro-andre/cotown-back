4ad6841c45f605c2cb41563f570c2279848f5614
alejandro-andre <alejandroandref@gmail.com>
2026-02-22 18:16:44 +0100

    Fix copy, remove MPR

--- etl/copy.sh
+++ etl/copy.sh
@@ -4,8 +4,8 @@ BACKUP_DIR="csv/$DATE_DIR"
 
 mkdir -p "$BACKUP_DIR"
 
-sed -e 's/ISA/XSA/g' -e 's/ISC/XSC/g' -e 's/Stabilised/Business Plan/g' csv/income_stabilised.csv > csv/income_business_plan.csv
-sed -e 's/OSA/XSA/g' -e 's/OSC/XSC/g' -e 's/Stabilised/Business Plan/g' csv/occupancy_stabilised.csv > csv/occupancy_business_plan.csv
-sed -e 's/BDR/XDR/g' -e 's/Real/Business Plan/g' csv/beds_real.csv > csv/beds_business_plan.csv
+sed -e '/^MP/d' -e 's/^ISA/XSA/' -e 's/^ISC/XSC/' -e 's/Stabilised/Business Plan/g' csv/income_stabilised.csv > csv/income_business_plan.csv
+sed -e 's/^OSA/XSA/' -e 's/^OSC/XSC/' -e 's/Stabilised/Business Plan/g' csv/occupancy_stabilised.csv > csv/occupancy_business_plan.csv
+sed -e 's/^BDR/XDR/' -e 's/Real/Business Plan/g' csv/beds_real.csv > csv/beds_business_plan.csv
 
 cp csv/income_business_plan.csv csv/occupancy_business_plan.csv csv/beds_business_plan.csv "$BACKUP_DIR/"
