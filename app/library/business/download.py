a330da69bad526361d928d36483a1fb74a3c323c
alejandro-andre <alejandroandref@gmail.com>
2026-02-24 21:33:07 +0100

    N2 changes v2

--- app/library/business/download.py
+++ app/library/business/download.py
@@ -204,6 +204,7 @@ def download_nra(dbClient, variables=None):
       substring(r."Registry_num", 11, 14) AS "CRU", 
       r."Registry_num" AS "NRUA",
       CASE 
+	    WHEN b."id" IS NULL THEN NULL
 	    WHEN b."Agent_id" IS NOT NULL THEN NULL
         WHEN b."Reason_id" IN (5)    THEN 1 -- Vacacional
         WHEN b."Reason_id" IN (2, 4) THEN 2 -- Laboral
@@ -211,26 +212,33 @@ def download_nra(dbClient, variables=None):
         ELSE NULL
       END AS "Reason", 
       CASE 
+	    WHEN b."id" IS NULL THEN NULL
 	    WHEN b."Agent_id" IS NOT NULL THEN NULL
         ELSE 1
       END AS "Pax",
       CASE 
+	    WHEN b."id" IS NULL THEN NULL
 	    WHEN b."Agent_id" IS NOT NULL THEN NULL
         ELSE GREATEST(b."Date_from"::date, make_date(%s, 1, 1))
       END AS "Date_from",
       CASE 
+	    WHEN b."id" IS NULL THEN NULL
 	    WHEN b."Agent_id" IS NOT NULL THEN NULL
         ELSE LEAST(b."Date_to"::date, make_date(%s, 12, 31)) 
       END AS "Date_to",
       a."Name"
-    FROM "Booking"."Booking" b 
-      INNER JOIN "Resource"."Resource" r ON r.id = b."Resource_id" 
-      INNER JOIN "Booking"."Customer_reason" cr ON cr.id = b."Reason_id" 
+    FROM "Resource"."Resource" r 
+      LEFT JOIN "Booking"."Booking" b ON r.id = b."Resource_id" 
+      LEFT JOIN "Booking"."Customer_reason" cr ON cr.id = b."Reason_id" 
       LEFT JOIN "Provider"."Agent" a ON a.id = b."Agent_id" 
-    WHERE b."Status" IN ('confirmada','firmacontrato','contrato','checkinconfirmado','checkin','inhouse','checkout','devolvergarantia','finalizada','revision')
-      AND b."Date_from"::date <= make_date(%s, 12, 31)
-      AND b."Date_to"::date   >= make_date(%s, 1, 1)
-    ORDER BY 3, 2, 1  '''
+    WHERE b."Status" IS NULL 
+      OR (
+      	b."Status" IN ('confirmada','firmacontrato','contrato','checkinconfirmado','checkin','inhouse','checkout','devolvergarantia','finalizada','revision')
+      	AND b."Date_from"::date <= make_date(%s, 12, 31)
+	    AND b."Date_to"::date   >= make_date(%s, 1, 1)
+	  )
+    ORDER BY 3, 2, 1
+  '''
 
   # Capture exceptions
   try:
@@ -253,10 +261,10 @@ def download_nra(dbClient, variables=None):
         # CSV Line
         line = ';'.join([
           nrua, 
-          item['Date_from'].strftime('%Y-%m-%d'), 
-          item['Date_to'].strftime('%Y-%m-%d'),
-          str(item['Pax']),
-          str(item['Reason'])
+          item['Date_from'].strftime('%Y-%m-%d') if item['Date_from'] else '', 
+          item['Date_to'].strftime('%Y-%m-%d') if item['Date_to'] else '',
+          str(item['Pax']) if item['Pax'] else '',
+          str(item['Reason']) if item['Reason'] else '',
         ])
 
         # Write CSV file
--- app/templates/report/nra.reservas.sql
+++ app/templates/report/nra.reservas.sql
@@ -1,23 +1,35 @@
 SELECT 
   b.id,
   r."Code",
-  a."Name" as "Marketplace",
   substring(r."Registry_num", 11, 14) AS "CRU", 
   r."Registry_num" AS "NRUA",
   CASE 
-	WHEN b."Reason_id" IN (5) THEN 1    -- Vacacional
-	WHEN b."Reason_id" IN (2, 4) THEN 2 -- Laboral
-	WHEN b."Reason_id" IN (1, 3) THEN 3 -- Estudios
+	  WHEN b."Reason_id" IN (5) THEN 1    -- Vacacional
+	  WHEN b."Reason_id" IN (2, 4) THEN 2 -- Laboral
+	  WHEN b."Reason_id" IN (1, 3) THEN 3 -- Estudios
   	ELSE NULL
   END AS "Reason", 
-  1 AS "Pax",
-  GREATEST(b."Date_from"::date, make_date(%(year)s, 1, 1)) AS "Date_from",
-  LEAST(b."Date_to"::date, make_date(%(year)s, 12, 31)) AS "Date_to"
-FROM "Booking"."Booking" b 
-  INNER JOIN "Resource"."Resource" r ON r.id = b."Resource_id" 
-  INNER JOIN "Booking"."Customer_reason" cr ON cr.id = b."Reason_id" 
+  CASE
+    WHEN b.id IS NULL THEN NULL
+    ELSE 1
+  END AS "Pax",
+  CASE
+    WHEN b.id IS NULL THEN NULL
+    ELSE GREATEST(b."Date_from"::date, make_date(%(year)s, 1, 1)) 
+  END AS "Date_from",
+  CASE
+    WHEN b.id IS NULL THEN NULL
+    ELSE LEAST(b."Date_to"::date, make_date(%(year)s, 12, 31)) 
+  END AS "Date_to",
+  a."Name" as "Marketplace"
+FROM "Resource"."Resource" r 
+  LEFT JOIN "Booking"."Booking" b ON r.id = b."Resource_id" 
+  LEFT JOIN "Booking"."Customer_reason" cr ON cr.id = b."Reason_id" 
   LEFT JOIN "Provider"."Agent" a ON a.id = b."Agent_id"
-WHERE b."Status" IN ('confirmada','firmacontrato','contrato','checkinconfirmado','checkin','inhouse','checkout','devolvergarantia','finalizada','revision')
-  AND b."Date_from"::date <= make_date(%(year)s, 12, 31)
-  AND b."Date_to"::date   >= make_date(%(year)s, 1, 1)
+WHERE b."Status" IS NULL 
+  OR (
+    b."Status" IN ('confirmada','firmacontrato','contrato','checkinconfirmado','checkin','inhouse','checkout','devolvergarantia','finalizada','revision')
+    AND b."Date_from"::date <= make_date(%(year)s, 12, 31)
+    AND b."Date_to"::date   >= make_date(%(year)s, 1, 1)
+  )
 ORDER BY 3, 2, 1
